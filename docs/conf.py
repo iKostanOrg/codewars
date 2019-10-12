ed3d5e333b92881d78957fb93d45f64aec50dbe3
Egor Kostan <igorkostan@gmail.com>
2019-10-11 18:43:29 -0700

    Update conf.py
    [C0111(missing-docstring)] Missing module docstring

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+"""
+Configuration file for the Sphinx documentation builder
+"""
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
