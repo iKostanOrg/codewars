f9b0042f66a57b61e21b5c3794ad64bf58352322
Egor Kostan <igorkostan@gmail.com>
2019-10-11 17:56:16 -0700

    You're a square! solution

---
+++ you_are_square/test_you_are_square.py
@@ -0,0 +1,61 @@
+import unittest
+from you_are_square.you_are_square import is_square
+
+
+class YouAreSquareTestCase(unittest.TestCase):
+	"""
+	Testing is_square function
+
+	The tests will always use some integral number,
+	so don't worry about that in dynamic typed languages.
+	"""
+
+	def test_is_square_negative_numbers(self):
+		"""
+		-1: Negative numbers cannot be square numbers
+		:return:
+		"""
+		self.assertEqual(is_square(-1),
+		                 False,
+		                 "-1: Negative numbers cannot be square numbers")
+
+	def test_is_square_zero(self):
+		"""
+		0 is a square number
+		:return:
+		"""
+		self.assertEqual(is_square(0),
+		                 True,
+		                 "0 is a square number")
+
+	def test_is_square_negative_test(self):
+		"""
+		3 is not a square number
+		:return:
+		"""
+		self.assertEqual(is_square(3),
+		                 False,
+		                 "3 is not a square number")
+
+	def test_is_square_four(self):
+		self.assertEqual(is_square(4),
+		                 True,
+		                 "4 is a square number")
+
+	def test_is_square_25(self):
+		"""
+		25 is a square number
+		:return:
+		"""
+		self.assertEqual(is_square(25),
+		                 True,
+		                 "25 is a square number")
+
+	def test_is_square_26(self):
+		"""
+		26 is not a square number
+		:return:
+		"""
+		self.assertEqual(is_square(26),
+		                 False,
+		                 "26 is not a square number")
---
+++ you_are_square/you_are_square.py
@@ -0,0 +1,18 @@
+import math
+
+
+def is_square(n) -> bool:
+    """
+    Given an integral number, determine if it's a square number:
+    :param n:
+    :return:
+    """
+    if n > -1:
+        number = math.sqrt(n)
+
+        if (number - int(number)) == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
