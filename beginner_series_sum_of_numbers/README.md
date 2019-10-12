0df4f80465fdedfea21eb80052ac342175d4809c
Egor Kostan <igorkostan@gmail.com>
2019-10-12 12:54:30 -0700

    Sum of numbers
    Some comments added

---
+++ beginner_series_sum_of_numbers/README.md
@@ -0,0 +1,18 @@
+## Beginner Series #3 Sum of Numbers
+
+Given two integers **a** and **b**, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return **a** or **b**.
+
+**Note:** a and b are not ordered!
+
+### Examples:
+
+```bash
+get_sum(1, 0) == 1   // 1 + 0 = 1
+get_sum(1, 2) == 3   // 1 + 2 = 3
+get_sum(0, 1) == 1   // 0 + 1 = 1
+get_sum(1, 1) == 1   // 1 Since both are same
+get_sum(-1, 0) == -1 // -1 + 0 = -1
+get_sum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+```
+
+[Source](https://www.codewars.com/kata/55f2b110f61eb01779000053/train/python)
---
+++ beginner_series_sum_of_numbers/test_sum_of_numbers.py
@@ -0,0 +1,35 @@
+import unittest
+from beginner_series_sum_of_numbers.sum_of_numbers import get_sum
+
+# FUNDAMENTALS ALGORITHMS
+
+
+class SumOfNumbersTestCase(unittest.TestCase):
+	"""
+	Testing get_sum function
+	"""
+
+	def test_get_sum_equal_numbers(self):
+		"""
+		a and b are equal
+		:return:
+		"""
+		self.assertEqual(get_sum(1, 1), 1)
+
+	def test_get_sum_positive_numbers(self):
+		"""
+		a an b are positive numbers
+		:return:
+		"""
+		self.assertEqual(get_sum(0, 1), 1)
+		self.assertEqual(get_sum(1, 0), 1)
+		self.assertEqual(get_sum(1, 2), 3)
+
+	def test_get_sum_negative_numbers(self):
+		"""
+		a or b is negative
+		:return:
+		"""
+		self.assertEqual(get_sum(0, -1), -1)
+		self.assertEqual(get_sum(-1, 0), -1)
+		self.assertEqual(get_sum(-1, 2), 2)
