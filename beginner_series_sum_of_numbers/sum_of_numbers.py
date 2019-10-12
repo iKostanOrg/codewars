6e18241233610332d2245b7cfd82e1e9eee47c20
Egor Kostan <igorkostan@gmail.com>
2019-10-12 14:28:46 -0700

    Update sum_of_numbers.py
    [W0312(mixed-indentation)] Found indentation with tabs instead of spaces

--- beginner_series_sum_of_numbers/sum_of_numbers.py
+++ beginner_series_sum_of_numbers/sum_of_numbers.py
@@ -1,16 +1,16 @@
 def get_sum(a, b):
-	"""
-	Given two integers a and b,
-	which can be positive or negative,
-	find the sum of all the numbers
-	between including them too and return it.
-	If the two numbers are equal return a or b.
-	:param a:
-	:param b:
-	:return:
-	"""
-	if a > b:
-		a, b = b, a
+    """
+    Given two integers a and b,
+    which can be positive or negative,
+    find the sum of all the numbers
+    between including them too and return it.
+    If the two numbers are equal return a or b.
+    :param a:
+    :param b:
+    :return:
+    """
+    if a > b:
+        a, b = b, a
 
-	nums = range(a, (b + 1))
-	return sum(nums)
+    nums = range(a, (b + 1))
+    return sum(nums)
