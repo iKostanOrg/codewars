f1dc7d0195859970e851519b99ad50ab6aa34a9f
Egor Kostan <igorkostan@gmail.com>
2019-10-12 21:28:27 -0700

    Log function

---
+++ utils/log_func.py
@@ -0,0 +1,18 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def print_log(**kwargs) -> None:
+	"""
+	Print log
+	:param args:
+	:return:
+	"""
+
+	log = ''
+	for key in kwargs:
+		log += '{}: {}, '.format(key, kwargs[key])
+
+	print('\nLOG =>\n{}\n'.format(log[:-2]))
+	return None
