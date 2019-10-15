3386422fe092153ee939e29078e4e92dc3a44abb
Egor Kostan <igorkostan@gmail.com>
2019-10-14 23:38:32 -0700

    8 kyu

---
+++ kyu_8/multiply/README.md
@@ -0,0 +1,12 @@
+## Multiply
+
+### Problem Description
+
+The code does not execute properly. Try to figure out why.
+
+```python
+def multiply(a, b):
+  a * b
+```
+
+[Source](https://www.codewars.com/kata/50654ddff44f800200000004/train/python)
---
+++ kyu_8/multiply/multiply.py
@@ -0,0 +1,26 @@
+"""
+Multiply Problem Description
+
+The code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+  a * b
+
+Source:
+https://www.codewars.com/kata/
+50654ddff44f800200000004/train/python
+"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def multiply(a, b):
+    """
+    Multiply two numbers and return the result
+    :param a:
+    :param b:
+    :return:
+    """
+    return a * b
