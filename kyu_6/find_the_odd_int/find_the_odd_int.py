58755f10fd789ba0a272606409c0829605d27247
Egor Kostan <igorkostan@gmail.com>
2019-10-15 14:28:58 -0700

    Update find_the_odd_int.py
    [W0312(mixed-indentation)] Found indentation with tabs instead of spaces

--- kyu_6/find_the_odd_int/find_the_odd_int.py
+++ kyu_6/find_the_odd_int/find_the_odd_int.py
@@ -6,20 +6,20 @@
 
 
 def find_it(seq) -> int:
-	"""
-	Given an array, find the int that
-	appears an odd number of times.
-	:param seq:
-	:return:
-	"""
-	pares = dict()
+    """
+    Given an array, find the int that
+    appears an odd number of times.
+    :param seq:
+    :return:
+    """
+    pares = dict()
 
-	for n in seq:
-		if n not in pares:
-			pares[n] = 1
-		else:
-			pares[n] = pares.get(n) + 1
+    for n in seq:
+        if n not in pares:
+            pares[n] = 1
+        else:
+            pares[n] = pares.get(n) + 1
 
-	for key in pares.keys():
-		if pares.get(key) % 2 > 0:
-			return key
+    for key in pares.keys():
+        if pares.get(key) % 2 > 0:
+            return key
