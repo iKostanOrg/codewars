6db52a82c9dbe9137374ae80230eed82606b1763
Egor Kostan <igorkostan@gmail.com>
2019-10-15 13:55:05 -0700

    Grasshopper - Messi goals function

---
+++ kyu_8/grasshopper_messi_goals_function/messi_goals_function.py
@@ -0,0 +1,20 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def goals(laLiga: int, copaDelRey: int, championsLeague: int) -> int:
+    """
+    The function returns Messi's total number
+    of goals in all three leagues:
+    - LaLiga
+    - Copa del Rey
+    - Champions
+
+    :param laLiga:
+    :param copaDelRey:
+    :param championsLeague:
+    :return:
+    """
+
+    return laLiga + copaDelRey + championsLeague
---
+++ kyu_8/grasshopper_messi_goals_function/test_messi_goals_function.py
@@ -0,0 +1,59 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+# FUNDAMENTALS
+
+import unittest
+import allure
+from utils.log_func import print_log
+from kyu_8.grasshopper_messi_goals_function.messi_goals_function import goals
+
+
+@allure.epic('8 kyu')
+@allure.parent_suite('Beginner')
+@allure.suite("Math")
+@allure.sub_suite("Unit Tests")
+@allure.feature("Addition")
+@allure.story('Personalized greeting')
+class GoalsTestCase(unittest.TestCase):
+	"""
+	Testing goals function
+	"""
+
+	def test_goals(self):
+		"""
+		Verify that the function returns Messi's
+		total number of goals in all three leagues.
+		:return:
+		"""
+		allure.dynamic.title("goals function verification")
+		allure.dynamic.severity(allure.severity_level.NORMAL)
+
+		with allure.step("Test with all zeroes"):
+			la_liga = 0
+			copa_del_rey = 0
+			champions = 0
+			expected = 0
+
+			print_log(la_liga=la_liga,
+			          copa_del_rey=copa_del_rey,
+			          champions=champions,
+			          expected=expected)
+
+		self.assertEqual(goals(la_liga, copa_del_rey, champions),
+		                 expected)
+
+		with allure.step("Test with positive integers"):
+			la_liga = 5
+			copa_del_rey = 10
+			champions = 2
+			expected = 17
+
+			print_log(la_liga=la_liga,
+			          copa_del_rey=copa_del_rey,
+			          champions=champions,
+			          expected=expected)
+
+		self.assertEqual(goals(la_liga, copa_del_rey, champions),
+		                 expected)
