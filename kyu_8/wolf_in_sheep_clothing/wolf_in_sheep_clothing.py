9880729c4d2389da1dba159bd6411806e4371549
Egor Kostan <igorkostan@gmail.com>
2019-10-17 14:13:38 -0700

    A wolf in sheep's clothing

---
+++ kyu_8/wolf_in_sheep_clothing/wolf_in_sheep_clothing.py
@@ -0,0 +1,32 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def warn_the_sheep(queue: list) -> str:
+    """
+    Warn the sheep in front of the wolf
+    that it is about to be eaten.
+
+    If the wolf is the closest animal to you,
+    return "Pls go away and stop eating my sheep".
+
+    Otherwise, return "Oi! Sheep number N!
+    You are about to be eaten by a wolf!"
+    where N is the sheep's position in the queue.
+
+    :param queue:
+    :return:
+    """
+
+    warning = "Pls go away and stop eating my sheep"
+    alert_start = "Oi! Sheep number "
+    alert_end = "! You are about to be eaten by a wolf!"
+    wolf = 'wolf'
+
+    if queue[-1] == wolf:
+        return warning
+    else:
+        index = len(queue) - queue.index(wolf) - 1
+
+    return '{}{}{}'.format(alert_start, index, alert_end)
