f227bec5fa76da3967d773181d2358133e44ffbf
Egor Kostan <igorkostan@gmail.com>
2019-10-21 13:48:41 -0700

    Allure report
    Numericals of a String

--- kyu_6/numericals_of_string/test_numericals.py
+++ kyu_6/numericals_of_string/test_numericals.py
@@ -13,7 +13,7 @@ from kyu_6.numericals_of_string.numericals import numericals
 @allure.suite("Algorithms")
 @allure.sub_suite("Unit Tests")
 @allure.feature("String")
-@allure.story('First character that repeats')
+@allure.story('Numericals of a String')
 class NumericalsTestCase(unittest.TestCase):
 	"""
 	Testing 'numericals' function
