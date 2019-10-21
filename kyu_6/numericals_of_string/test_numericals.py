6d145b98b564e05c3e49eba1bb7425eb66b02466
Egor Kostan <igorkostan@gmail.com>
2019-10-21 13:43:49 -0700

    Numericals of a String

---
+++ kyu_6/numericals_of_string/test_numericals.py
@@ -0,0 +1,69 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import unittest
+import allure
+from utils.log_func import print_log
+from kyu_6.numericals_of_string.numericals import numericals
+
+
+@allure.epic('6 kyu')
+@allure.parent_suite('Novice')
+@allure.suite("Algorithms")
+@allure.sub_suite("Unit Tests")
+@allure.feature("String")
+@allure.story('First character that repeats')
+class NumericalsTestCase(unittest.TestCase):
+	"""
+	Testing 'numericals' function
+	"""
+
+	def test_numericals(self):
+		"""
+		Testing 'numericals' function
+		:return:
+		"""
+
+		allure.dynamic.title("Testing 'numericals' function")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+
+		with allure.step("Pass the string and verify the output"):
+			string = "Hello, World!"
+			expected = "1112111121311"
+
+			print_log(string=string, expected=expected)
+
+			self.assertEqual(numericals(string), expected)
+
+		with allure.step("Pass the string and verify the output"):
+			string = "Hello, World! It's me, JomoPipi!"
+			expected = "11121111213112111131224132411122"
+
+			print_log(string=string, expected=expected)
+
+			self.assertEqual(numericals(string), expected)
+
+		with allure.step("Pass the string and verify the output"):
+			string = "hello hello"
+			expected = "11121122342"
+
+			print_log(string=string, expected=expected)
+
+			self.assertEqual(numericals(string), expected)
+
+		with allure.step("Pass the string and verify the output"):
+			string = "Hello"
+			expected = "11121"
+
+			print_log(string=string, expected=expected)
+
+			self.assertEqual(numericals(string), expected)
+
+		with allure.step("Pass the string and verify the output"):
+			string = "aaaaaaaaaaaa"
+			expected = "123456789101112"
+
+			print_log(string=string, expected=expected)
+
+			self.assertEqual(numericals(string), expected)
