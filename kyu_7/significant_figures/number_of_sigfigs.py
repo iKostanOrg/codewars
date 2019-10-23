0ba55c7689ba12859dd9f2f7d2eaf7d584e4f6c1
Egor Kostan <igorkostan@gmail.com>
2019-10-23 15:50:26 -0700

    Significant Figures

---
+++ kyu_7/significant_figures/number_of_sigfigs.py
@@ -0,0 +1,83 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def number_of_sigfigs(number: str) -> int:
+    """
+    return the number of sigfigs in the
+    passed in string "number"
+    :param number:
+    :return:
+    """
+
+    number = normalize_string(number)
+    
+    if number == '0':
+        return 0
+    
+    if number == '0.0':
+        return 1
+
+    result = 0
+    for i, char in enumerate(number):
+
+        if char.isdigit() and not (i == 0 and char == '0'):
+            result += 1
+
+    return result
+
+
+def normalize_string(number: str) -> str:
+    """
+    Normalize string by converting it into a
+    number and back to string once again
+    :param number:
+    :return:
+    """
+
+    if '.' not in number:
+        number = str(int(number))
+        number = remove_extra_zeroes(number)
+    else:
+        number = remove_extra_leading_zeroes(number)
+
+    return number
+
+
+def remove_extra_zeroes(number: str) -> str:
+    """
+    Remove all zeroes from the end of the string
+    :param number:
+    :return:
+    """
+
+    index = None
+
+    for i in range(-1, len(number) * -1, -1):
+        if number[i] == '0':
+            index = i
+        else:
+            break
+
+    if index is not None:
+        return number[0:index]
+
+    return number
+
+
+def remove_extra_leading_zeroes(number: str) -> str:
+    """
+    Remove all extra leading zeroes from the head of the string
+    :param number:
+    :return:
+    """
+    new_number = str(float(number))
+
+    after_dot = len(number[number.index('.'):])
+    new_after_dot = len(new_number[new_number.index('.'):])
+
+    if after_dot == new_after_dot:
+        return new_number
+    else:
+        return new_number + ('0' * (after_dot - new_after_dot))
---
+++ kyu_7/significant_figures/test_number_of_sigfigs.py
@@ -0,0 +1,55 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+# ALGORITHMS MATHEMATICS NUMBERS STRINGS
+
+import unittest
+import allure
+from utils.log_func import print_log
+from kyu_7.significant_figures.number_of_sigfigs import number_of_sigfigs
+
+
+@allure.epic('7 kyu')
+@allure.parent_suite('Beginner')
+@allure.suite("Algorithms")
+@allure.sub_suite("Unit Tests")
+@allure.feature("String")
+@allure.story('Significant Figures')
+class NumberOfSigFigsTestCase(unittest.TestCase):
+	"""
+	Testing number_of_sigfigs function
+	"""
+
+	def test_number_of_sigfigs(self):
+		"""
+		Testing number_of_sigfigs function
+		with various test inputs
+		:return:
+		"""
+
+		allure.dynamic.title('Testing number_of_sigfigs function')
+		allure.dynamic.severity(allure.severity_level.CRITICAL)
+
+		with allure.step("Pass string and verify the output"):
+			TESTS = [
+				(1, "1"),
+				(0, "0"),
+				(1, "0003"),
+				(1, "3000"),
+				(3, "404"),
+				(7, "050030210"),
+				(1, "0.1"),
+				(2, '1.0'),
+				(3, '4.40'),
+				(4, '90.00'),
+				(1, "0.0"),
+				(9, '03.27310000'),
+				(10, '23625700.00'),
+				(10, '09.971730000'),
+				(10, '0000.0673560000')
+			]
+
+			for exp, inp in TESTS:
+				print_log(inp=inp, expected=exp)
+				self.assertEqual(exp, number_of_sigfigs(inp))
