08881bc7c2140f073b53c3627e5350146b1c277e
Egor Kostan <igorkostan@gmail.com>
2019-10-23 18:40:33 -0700

    Unique In Order

---
+++ kyu_6/unique_in_order/unique_in_order.py
@@ -0,0 +1,23 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def unique_in_order(iterable) -> list:
+    """
+    Takes as argument a sequence and returns a list
+    of items without any elements with the same value
+    next to each other and preserving the original
+    order of elements.
+    :param iterable:
+    :return:
+    """
+
+    result = []
+
+    for i in iterable:
+
+        if len(result) == 0 or i != result[-1]:
+            result.append(i)
+
+    return result
