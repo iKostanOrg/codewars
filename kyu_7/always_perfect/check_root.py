8a0094eb5abdc01237e6b69adb25f8475c6090d4
Egor Kostan <igorkostan@gmail.com>
2019-10-30 12:13:54 -0700

    Always perfect

---
+++ kyu_7/always_perfect/check_root.py
@@ -0,0 +1,44 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import math
+
+
+def check_root(string: str) -> str:
+    """
+    A function which takes numbers separated by commas
+    in string format and returns the number which is a
+    perfect square and the square root of that number.
+
+    If string contains other characters than number or
+    it has more or less than 4 numbers separated by comma
+    function returns "incorrect input".
+
+    If string contains 4 numbers but not consecutive it
+    returns "not consecutive".
+    :param string:
+    :return:
+    """
+
+    string_arr = string.split(',')
+    if len(string_arr) != 4:
+        return 'incorrect input'
+
+    try:
+        string_arr = list(int(char) for char in string_arr)
+    except ValueError:
+        return 'incorrect input'
+
+    result = 1
+    for s in string_arr:
+        result *= s
+
+    result += 1
+
+    root = math.sqrt(result)
+
+    if root % 1 == 0:
+        return '{}, {}'.format(result, int(root))
+    else:
+        return 'not consecutive'
---
+++ kyu_7/always_perfect/test_check_root.py
@@ -0,0 +1,57 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+# FUNDAMENTALS STRINGS STRING FORMATTING FORMATTING ALGORITHMS ARRAYS MATHEMATICS
+
+import unittest
+import allure
+from utils.log_func import print_log
+from kyu_7.always_perfect.check_root import check_root
+
+
+@allure.epic('7 kyu')
+@allure.parent_suite('Beginner')
+@allure.suite("Data Structures")
+@allure.sub_suite("Unit Tests")
+@allure.feature("Lists")
+@allure.story('Always perfect')
+class CheckRootTestCase(unittest.TestCase):
+	"""
+	Testing check_root function
+	"""
+
+	def test_check_root(self):
+		"""
+		Testing check_root function with various test inputs
+
+		A function which takes numbers separated by commas
+		in string format and returns the number which is a
+		perfect square and the square root of that number.
+
+		If string contains other characters than number or
+		it has more or less than 4 numbers separated by comma
+		function returns "incorrect input".
+
+		If string contains 4 numbers but not consecutive it
+		returns "not consecutive".
+		:return:
+		"""
+
+		allure.dynamic.title("Testing check_root function")
+		allure.dynamic.severity(allure.severity_level.CRITICAL)
+
+		with allure.step("Enter test string and verify the output"):
+
+			data = [
+				('4,5,6,7', '841, 29'),
+				('3,s,5,6', 'incorrect input'),
+				('11,13,14,15', 'not consecutive'),
+				('10,11,12,13,15', 'incorrect input'),
+				('10,11,12,13', '17161, 131'),
+				('*-3,-2,-1,0', 'incorrect input'),
+			]
+
+			for string, expected in data:
+				print_log(string=string, expected=expected)
+				self.assertEqual(expected, check_root(string))
