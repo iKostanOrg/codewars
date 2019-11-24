3039031d50d37d5cce74e5d70e9f584fdd557e5d
Egor Kostan <igorkostan@gmail.com>
2019-11-24 09:24:34 -0800

    Update test_hoop_count.py

--- kyu_8/keep_up_the_hoop/test_hoop_count.py
+++ kyu_8/keep_up_the_hoop/test_hoop_count.py
@@ -17,13 +17,29 @@ from kyu_8.keep_up_the_hoop.hoop_count import hoop_count
 @allure.feature("Conditions")
 @allure.story('Keep up the hoop')
 class HoopCountTestCase(unittest.TestCase):
+	"""
+	Testing hoop_count function
+	"""
 
 	def test_hoop_count_positive(self):
+		"""
+		Testing hoop_count function
 
-		allure.dynamic.title("")
+		Alex just got a new hula hoop, he loves it but feels
+		discouraged because his little brother is better than him
+
+		Write a program where Alex can input (n) how many times
+		the hoop goes round and it will return him an encouraging message
+
+		- If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+		- If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+		:return:
+		"""
+
+		allure.dynamic.title("Testing hoop_count function (positive test case)")
 		allure.dynamic.severity(allure.severity_level.NORMAL)
 
-		with allure.step(""):
+		with allure.step("Enter n and verify the result"):
 			n = 11
 			expected = "Great, now move on to tricks"
 			print_log(n=n, expected=expected)
@@ -31,10 +47,10 @@ class HoopCountTestCase(unittest.TestCase):
 
 	def test_hoop_count_negative(self):
 
-		allure.dynamic.title("")
+		allure.dynamic.title("Testing hoop_count function (negative test case)")
 		allure.dynamic.severity(allure.severity_level.NORMAL)
 
-		with allure.step(""):
+		with allure.step("Enter n and verify the result"):
 			n = 3
 			expected = "Keep at it until you get it"
 			print_log(n=n, expected=expected)
