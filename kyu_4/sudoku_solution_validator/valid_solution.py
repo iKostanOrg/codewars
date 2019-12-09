1948c40f22fbdf4a6a9fe80880a90f73e23e9be0
Egor Kostan <igorkostan@gmail.com>
2019-12-08 18:40:49 -0800

    Sudoku Solution Validator -> start a new problem

---
+++ kyu_4/sudoku_solution_validator/valid_solution.py
@@ -0,0 +1,12 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def validSolution(board: list) -> bool:
+	"""
+
+	:param board:
+	:return:
+	"""
+	pass
