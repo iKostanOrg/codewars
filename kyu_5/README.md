5072a32842f4cdb5db6c8b45c4b5eacff5d0b4b0
Egor Kostan <igorkostan@gmail.com>
2019-12-22 15:54:58 -0800

    Moving Zeros To The End
    Moving Zeros To The End -> solved

--- kyu_5/README.md
+++ kyu_5/README.md
@@ -22,5 +22,6 @@ You can advance through the ranks by completing kata at or above your rank - the
 |4    |[Human Readable Time](https://www.codewars.com/kata/52685f7382004e774f0001f7/train/python)                          |[Solution](https://github.com/ikostan/codewars/tree/master/kyu_5/human_readable_time)            |
 |5    |[Alphabet wars - nuclear strike](https://www.codewars.com/kata/alphabet-wars-nuclear-strike/train/python)           |[Solution](https://github.com/ikostan/codewars/tree/master/kyu_5/alphabet_wars_nuclear_strike)   |
 |6    |[Valid Parentheses](https://www.codewars.com/kata/52774a314c2333f0a7000688/train/python)                            |[Solution](https://github.com/ikostan/codewars/tree/master/kyu_5/valid_parentheses)              |
+|7    |[Moving Zeros To The End](https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python)                      |[Solution](https://github.com/ikostan/codewars/tree/master/kyu_5/moving_zeros_to_the_end)        |
 
 [Source](https://www.codewars.com/about)
---
+++ kyu_5/moving_zeros_to_the_end/test_move_zeros.py
@@ -0,0 +1,56 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+# ALGORITHMS INTERVIEW QUESTIONS ARRAYS SORTING
+
+import unittest
+import allure
+from kyu_5.moving_zeros_to_the_end.move_zeros import move_zeros
+from utils.log_func import print_log
+
+
+@allure.epic('5 kyu')
+@allure.parent_suite('Novice')
+@allure.suite("Algorithms")
+@allure.sub_suite("Unit Tests")
+@allure.feature("Lists")
+@allure.story('Moving Zeros To The End')
+class MoveZerosTestCase(unittest.TestCase):
+	"""
+	Testing move_zeros function
+	"""
+
+	def test_move_zeros(self):
+		"""
+		Test an algorithm that takes an array and moves all of the
+		zeros to the end, preserving the order of the other elements.
+		:return:
+		"""
+
+		allure.dynamic.title("Testing move_zeros function")
+		allure.dynamic.severity(allure.severity_level.NORMAL)
+
+		with allure.step("Enter test data (list) and verify the output"):
+			data = [
+				([1, 2, 0, 1, 0, 1, 0, 3, 0, 1], [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]),
+				([9, 0.0, 0, 9, 1, 2, 0, 1, 0, 1, 0.0, 3, 0, 1, 9, 0, 0, 0, 0, 9],
+				 [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				(["a", 0, 0, "b", "c", "d", 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9],
+				 ["a", "b", "c", "d", 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				(["a", 0, 0, "b", None, "c", "d", 0, 1, False, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9],
+				 ["a", "b", None, "c", "d", 1, False, 1, 3, [], 1, 9, {}, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				([0, 1, None, 2, False, 1, 0], [1, None, 2, False, 1, 0, 0]),
+				(["a", "b"], ["a", "b"]),
+				(["a"], ["a"]),
+				([0, 0], [0, 0]),
+				([0], [0]),
+				([False], [False]),
+				([], []),
+			]
+
+			for d in data:
+				array = d[0]
+				expected = d[1]
+				print_log(array=array, expected=expected)
+				self.assertEqual(expected, move_zeros(array))
