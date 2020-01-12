db0aa17525f6887542aee9e1fe8c158a63a44e60
Egor Kostan <igorkostan@gmail.com>
2020-01-11 22:35:34 -0800

    Update test_format_duration.py
    allure.dynamic.description_html

--- kyu_4/human_readable_duration_format/test_format_duration.py
+++ kyu_4/human_readable_duration_format/test_format_duration.py
@@ -20,45 +20,58 @@ from kyu_4.human_readable_duration_format.format_duration import format_duration
 @allure.link(url='https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python',
              name='Source/Kata')
 class FormatDurationTestCase(unittest.TestCase):
-	"""
-	Testing format_duration
-	"""
+    """
+    Testing format_duration
+    """
 
-	def test_format_duration(self):
-		"""
-		Test a function which formats a duration,
-		given as a number of seconds, in a human-friendly way.
+    def test_format_duration(self):
+        """
+        Test a function which formats a duration,
+        given as a number of seconds, in a human-friendly way.
 
-		The function must accept a non-negative integer.
-		If it is zero, it just returns "now". Otherwise,
-		the duration is expressed as a combination of years,
-		days, hours, minutes and seconds.
-		:return:
-		"""
+        The function must accept a non-negative integer.
+        If it is zero, it just returns "now". Otherwise,
+        the duration is expressed as a combination of years,
+        days, hours, minutes and seconds.
+        :return:
+        """
 
-		allure.dynamic.title("Testing format_duration")
-		allure.dynamic.severity(allure.severity_level.NORMAL)
+        allure.dynamic.title("Testing format_duration")
+        allure.dynamic.severity(allure.severity_level.NORMAL)
+        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
+                                        '<img src="https://www.codewars.com/users/myFirstCode'
+                                        '/badges/large">'
+                                        '<h3>Test Description:</h3>'
+                                        "<p>Test a function which formats a duration, given as a "
+                                        "number of seconds, in a human-friendly way.</p>"
+                                        "<p>The function must accept a non-negative integer. If it is zero, "
+                                        "it just returns \"now\". Otherwise, the duration is expressed as a "
+                                        "combination of years, days, hours, minutes and seconds.</p>")
 
-		with allure.step("Enter seconds and verify the output"):
-			test_data = [
-				(1, "1 second"),
-				(62, "1 minute and 2 seconds"),
-				(120, "2 minutes"),
-				(3600, "1 hour"),
-				(3662, "1 hour, 1 minute and 2 seconds"),
-				(15731080, '182 days, 1 hour, 44 minutes and 40 seconds'),
-				(132030240, '4 years, 68 days, 3 hours and 4 minutes'),
-				(205851834, '6 years, 192 days, 13 hours, 3 minutes and 54 seconds'),
-				(253374061, '8 years, 12 days, 13 hours, 41 minutes and 1 second'),
-				(242062374, '7 years, 246 days, 15 hours, 32 minutes and 54 seconds'),
-				(101956166, '3 years, 85 days, 1 hour, 9 minutes and 26 seconds'),
-				(33243586, '1 year, 19 days, 18 hours, 19 minutes and 46 seconds'),
-				(9041160, '104 days, 15 hours and 26 minutes'),
-			]
+        test_data = [
+                (1, "1 second"),
+                (62, "1 minute and 2 seconds"),
+                (120, "2 minutes"),
+                (3600, "1 hour"),
+                (3662, "1 hour, 1 minute and 2 seconds"),
+                (15731080, '182 days, 1 hour, 44 minutes and 40 seconds'),
+                (132030240, '4 years, 68 days, 3 hours and 4 minutes'),
+                (205851834, '6 years, 192 days, 13 hours, 3 minutes and 54 seconds'),
+                (253374061, '8 years, 12 days, 13 hours, 41 minutes and 1 second'),
+                (242062374, '7 years, 246 days, 15 hours, 32 minutes and 54 seconds'),
+                (101956166, '3 years, 85 days, 1 hour, 9 minutes and 26 seconds'),
+                (33243586, '1 year, 19 days, 18 hours, 19 minutes and 46 seconds'),
+                (9041160, '104 days, 15 hours and 26 minutes'),
+            ]
 
-			for seconds, expected in test_data:
-				print_log(seconds=seconds,
-				          expected=expected)
+        for seconds, expected in test_data:
+            with allure.step("Enter seconds: {} and verify the output:".format(seconds)):
 
-				self.assertEqual(expected,
-				                 format_duration(seconds))
+                actual_result = format_duration(seconds)
+
+                print_log(seconds=seconds,
+                          expected=expected,
+                          result=actual_result)
+
+                self.assertEqual(expected,
+                                 actual_result)
