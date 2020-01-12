3759b55f0a489e3b582c86fc862ba3aa1b15e351
Egor Kostan <igorkostan@gmail.com>
2020-01-11 22:46:40 -0800

    Update test_next_bigger.py

--- kyu_4/next_bigger_number_with_the_same_digits/test_next_bigger.py
+++ kyu_4/next_bigger_number_with_the_same_digits/test_next_bigger.py
@@ -23,39 +23,50 @@ from kyu_4.next_bigger_number_with_the_same_digits.next_bigger import next_bigge
 @pytest.mark.skip(reason="The solution is not ready")
 class NextBiggerTestCase(unittest.TestCase):
 
-	def test_next_bigger(self):
-		"""
-		Testing next_bigger function
+    def test_next_bigger(self):
+        """
+        Testing next_bigger function
 
-		You have to test a function that takes a positive integer
-		number and returns the next bigger number formed by the same digits:
+        You have to test a function that takes a positive integer
+        number and returns the next bigger number formed by the same digits:
 
-		12 ==> 21
-		513 ==> 531
-		2017 ==> 2071
+        12 ==> 21
+        513 ==> 531
+        2017 ==> 2071
 
-		If no bigger number can be composed using those digits, return -1:
-		"""
+        If no bigger number can be composed using those digits, return -1
+        """
 
-		allure.dynamic.title("Testing next_bigger function")
-		allure.dynamic.severity(allure.severity_level.NORMAL)
+        allure.dynamic.title("Testing next_bigger function")
+        allure.dynamic.severity(allure.severity_level.NORMAL)
+        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
+                                        '<img src="https://www.codewars.com/users/myFirstCode'
+                                        '/badges/large">'
+                                        '<h3>Test Description:</h3>'
+                                        "<p>Testing next_bigger function</p>"
+                                        "<p>You have to test a function that takes a positive integer "
+                                        "number and returns the next bigger number formed by the same digits:</p>"
+                                        "<p>12 ==> 21"
+                                        "513 ==> 531"
+                                        "2017 ==> 2071</p>"
+                                        "<p>If no bigger number can be composed using those digits, return -1</p>")
 
-		with allure.step("Enter an integer and "
-		                 "calculate the result"):
-			data = [
-				(12, 21),
-				(513, 531),
-				(2017, 2071),
-				(414, 441),
-				(144, 414),
-			]
+        data = [
+                (12, 21),
+                (513, 531),
+                (2017, 2071),
+                (414, 441),
+                (144, 414),
+            ]
 
-			for d in data:
-				n = d[0]
-				expected = d[1]
-
-				print_log(n=n,
-				          expected=expected)
-
-				self.assertEqual(expected,
-				                 next_bigger(n))
+        for d in data:
+            n = d[0]
+            expected = d[1]
+            print_log(n=n, expected=expected)
+            actual_result = next_bigger(n)
+            with allure.step("Enter an integer ({}), "
+                             "calculate the result ({}) and "
+                             "compare it with expected ({})".format(n,
+                                                                    actual_result,
+                                                                    expected)):
+                self.assertEqual(expected, actual_result)
