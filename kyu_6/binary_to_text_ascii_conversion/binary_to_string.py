c0d1aff6a0684bf5f2666d65e86caa1976c08a03
Egor Kostan <igorkostan@gmail.com>
2020-01-18 22:41:12 -0800

    Update binary_to_string.py

--- kyu_6/binary_to_text_ascii_conversion/binary_to_string.py
+++ kyu_6/binary_to_text_ascii_conversion/binary_to_string.py
@@ -36,5 +36,5 @@ def binary_to_string(binary: str) -> str:
     byte_number = (binary_int.bit_length() + 7) // 8
     binary_array = binary_int.to_bytes(byte_number, "big")
     ascii_text = binary_array.decode()
-    
+
     return ascii_text
