3b503d678d30bcf621176507f361cff391422a7e
Egor Kostan <igorkostan@gmail.com>
2020-02-01 12:17:58 -0800

    Create josephus_survivor.py

---
+++ kyu_5/josephus_survivor/josephus_survivor.py
@@ -0,0 +1,10 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def josephus_survivor(n, k):
+
+    n_list = [i for i in range(1, n + 1)]
+
+    return n_list[0]
