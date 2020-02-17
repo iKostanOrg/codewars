600bbe83f3a35f709b9bd1eee0b9a655846f05db
Egor Kostan <igorkostan@gmail.com>
2020-02-16 19:08:41 -0800

    Update is_prime.py
    Avoid too many return statements within this function.

--- utils/is_prime.py
+++ utils/is_prime.py
@@ -12,18 +12,19 @@ def is_prime(n: int) -> bool:
     :param n:
     :return:
     """
+    result = True
 
     if n == 1:
-        return False
+        result = False
 
     if n == 2:
-        return True
+        result = False
 
     if n % 2 == 0 and n > 2:
-        return False
+        result = False
 
     for x in range(3, int(math.sqrt(n)) + 1, 2):
         if n % x == 0:
-            return False
+            result = False
 
-    return True
+    return result
