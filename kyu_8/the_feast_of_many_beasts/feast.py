a437204f0e29552883cd98437272fd4a40052de3
Egor Kostan <igorkostan@gmail.com>
2020-03-07 14:04:30 -0800

    Update feast.py

--- kyu_8/the_feast_of_many_beasts/feast.py
+++ kyu_8/the_feast_of_many_beasts/feast.py
@@ -10,11 +10,11 @@ def feast(beast: str, dish: str) -> bool:
     indicate whether the beast is allowed to bring the
     dish to the feast.
 
-	Assume that beast and dish are always lowercase strings,
-	and that each has at least two letters. beast and dish
-	may contain hyphens and spaces, but these will not appear
-	at the beginning or end of the string. They will not
-	contain numerals.
+    Assume that beast and dish are always lowercase strings,
+    and that each has at least two letters. beast and dish
+    may contain hyphens and spaces, but these will not appear
+    at the beginning or end of the string. They will not
+    contain numerals.
     :param beast:
     :param dish:
     :return:
