fedf67d0f84abf29ba085a6ba812b55780e57f2e
Egor Kostan <igorkostan@gmail.com>
2020-04-05 14:06:19 -0700

    Update easyline.py

--- kyu_7/easy_line/easyline.py
+++ kyu_7/easy_line/easyline.py
@@ -2,20 +2,40 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
+import math
+
 
 def easy_line(n: int) -> int:
     """
     The function will take n (with: n>= 0) as parameter
     and will return the sum of the squares of the binomial
     coefficients on line n.
-    :param n:
+
+    :param n: the line number (with: n>= 0)
     :return:
     """
-    coefficient: int = 1
-    total: int = 0
+    if n < 0:
+        raise ValueError('ERROR: invalid n ({}) value. n must be >= 0')
 
-    for i in range(n + 1):
-        total += coefficient ** 2
-        coefficient = coefficient * (n - i) // (i + 1)
+    if n == 0:
+        return 1
 
-    return total
+    result = 0
+    i = 0
+    for row in range(n - 1, 2 * n):
+        result += calc_combination_per_row_item(row, i)
+        i += 1
+
+    return result
+
+
+def calc_combination_per_row_item(row: int, i: int) -> int:
+    """
+    Generates a specific combination from Pascal's Triangle row by specified index
+    :param row: row
+    :param i: index
+    :return:
+    """
+    combination = int(math.factorial(row)) // (int(math.factorial(i)) * int(math.factorial(row - i)))
+    # print(f'\n{row}! / ({i}! * ({row} - {i})!) = {combination}') # debug only
+    return combination
