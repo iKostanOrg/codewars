a4f29a0dc88e7b0acc129acfbc972602aec60ab7
Egor Kostan <igorkostan@gmail.com>
2020-04-04 22:22:08 -0700

    Update test_advice.py

--- kyu_5/find_the_safest_places_in_town/test_advice.py
+++ kyu_5/find_the_safest_places_in_town/test_advice.py
@@ -6,6 +6,8 @@
 
 import unittest
 import allure
+
+from kyu_5.find_the_safest_places_in_town.print_agents import print_map
 from utils.log_func import print_log
 from kyu_5.find_the_safest_places_in_town.advice import advice
 
@@ -65,6 +67,18 @@ class FirstNonRepeatingLetterTestCase(unittest.TestCase):
                  "Single agent in top left corner of large grid"),
                 ([(0, 0), (1, 1), (99, 99)], 2, sorted([(0, 1), (1, 0)]),
                  "Agent in top left and bottom right"),
+                ([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (1, 0), (1, 1),
+                  (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (2, 0), (2, 1), (2, 2), (2, 3),
+                  (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
+                  (3, 6), (3, 7), (3, 8), (3, 9), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7),
+                  (4, 8), (4, 9), (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9),
+                  (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9), (7, 0), (7, 1),
+                  (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8), (7, 9), (8, 0), (8, 1), (8, 2), (8, 3),
+                  (8, 4), (8, 5), (8, 6), (8, 7), (8, 8), (8, 9), (9, 0), (9, 1), (9, 2), (9, 3), (9, 4), (9, 5),
+                  (9, 6), (9, 7), (9, 8), (9, 9)], 10, [],
+                 "Returns empty list for agents everywhere"),
+                ([(12, 8), (0, 14), (10, 16), (7, 11)], 2, [(0, 0), (0, 1), (1, 0), (1, 1)],
+                 'No agents on map')
             ]
 
             for data in test_data:
@@ -72,7 +86,10 @@ class FirstNonRepeatingLetterTestCase(unittest.TestCase):
                 n = data[1]
                 expected = data[2]
                 message = data[3]
+
                 print_log(agents=agents, n=n, expected=expected, message=message)
+                print_map(agents, n, expected)
+
                 self.assertEqual(expected,
                                  sorted(advice(agents=agents, n=n)),
                                  message)
