dbae509f35e9d4c42feb9905f8c73f522eedda36
Egor Kostan <igorkostan@gmail.com>
2020-04-07 23:30:30 -0700

    DefaultList

---
+++ kyu_6/default_list/README.md
@@ -0,0 +1,18 @@
+### DefaultList
+
+The collections module has defaultdict, which gives you a default value for trying to get the value of a key which does not exist in the dictionary instead of raising an error. Why not do this for a list?
+
+Your job is to create a class (or a function which returns an object) called DefaultList. The class will have two parameters to be given: a list, and a default value. The list will obviously be the list that corresponds to that object. The default value will be returned any time an index of the list is called in the code that would normally raise an error (i.e. i > len(list) - 1 or i < -len(list)). This class must also support the regular list functions extend, append, insert, remove, and pop.
+
+Because slicing a list never raises an error (slicing a list between two indexes that are not a part of the list returns [], slicing will not be tested for.
+
+Example:
+```text
+lst = DefaultList(['hello', 'abcd', '123', 123, True, False], 'default_value')
+lst[4] = True
+lst[80] = 'default_value'
+lst.extend([104, 1044, 4066, -2])
+lst[9] = -2
+```
+
+[Source](https://www.codewars.com/kata/5e882048999e6c0023412908/train/python)
---
+++ kyu_6/default_list/default_list.py
@@ -0,0 +1,65 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class DefaultList:
+
+    def __init__(self, lst: list, default_value: str):
+        # The class will have two parameters to be given:
+        # a list, and a default value.
+        self.__lst: list = lst
+        self.__default_value: str = default_value
+
+    def __getitem__(self, i: int):
+        """
+        The default value will be returned any time an index of the list
+        is called in the code that would normally raise an error:
+        (i.e. i > len(list) - 1 or i < -len(list)).
+        :param i:
+        :return:
+        """
+        if i >= len(self.__lst) or i < -len(self.__lst):
+            return self.__default_value
+        return self.__lst[i]
+
+    def extend(self, items: iter) -> None:
+        """
+        This class must also support the regular list functions: extend.
+        :param items: iterable
+        :return:
+        """
+        self.__lst.extend(items)
+
+    def append(self, item) -> None:
+        """
+        This class must also support the regular list functions: append.
+        :param item:
+        :return:
+        """
+        self.__lst.append(item)
+
+    def pop(self, item):
+        """
+        This class must also support the regular list functions: pop.
+        :param item:
+        :return:
+        """
+        return self.__lst.pop(item)
+
+    def remove(self, item) -> None:
+        """
+        This class must also support the regular list functions: remove.
+        :param item:
+        :return:
+        """
+        self.__lst.remove(item)
+
+    def insert(self, index: int, item) -> None:
+        """
+        This class must also support the regular list functions: insert.
+        :param index:
+        :param item:
+        :return:
+        """
+        self.__lst.insert(index, item)
