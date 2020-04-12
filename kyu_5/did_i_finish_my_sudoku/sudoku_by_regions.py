dfa4c8811fed1a317968e2321ec877e6ea36ac83
Egor Kostan <igorkostan@gmail.com>
2020-04-12 15:21:30 -0700

    Update sudoku_by_regions.py

--- kyu_5/did_i_finish_my_sudoku/sudoku_by_regions.py
+++ kyu_5/did_i_finish_my_sudoku/sudoku_by_regions.py
@@ -1,32 +1,41 @@
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
 from utils.primes.primes_generator import gen_primes
 
 
 def assert_sudoku_by_region(board: list) -> bool:
 	row_length = len(board[0])
-	start, end = 0, 3
 
 	step = 0
 	for i in gen_primes():
 		if row_length % i == 0:
 			step = i
 			break
+	print('Step: {}'.format(step))
+
+	start, end = 0, step
+
+	# limit = (row_length // step)  # + 1 if step % 2 != 0 else 0
+	# print('limit: {}'.format(limit))
 
 	for t in range(0, step):
-		# print()
-		for i in range(0, row_length - step + 1 if step % 2 != 0 else 0, step):
-			region = set()
+		print()
+		for i in range(0, row_length, step):
+			region = list()
 			for a in range(i, i + step):
+				print('a: {}, start: {}, end: {}'.format(a, start, end))
 				row = board[a][start: end]
 				for b in row:
-					region.add(b)
-			# print(region)
-			if len(region) != row_length:
-				# print('assert_sudoku_by_region')
+					region.append(b)
+			print(region)
+			s_region = set(region)
+			if len(s_region) != row_length:
+				print(s_region)
+				print('assert_sudoku_by_region')
 				return False
 
-		start, end = end, end + 3
+		start, end = end, end + step
 
 	return True
