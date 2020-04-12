bd0825b360546985eb1675cf7d7259f0ecf6da31
Egor Kostan <igorkostan@gmail.com>
2020-04-12 12:33:16 -0700

    Update sudoku_by_regions.py

--- kyu_5/did_i_finish_my_sudoku/sudoku_by_regions.py
+++ kyu_5/did_i_finish_my_sudoku/sudoku_by_regions.py
@@ -1,21 +1,28 @@
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+from utils.primes.primes_generator import gen_primes
 
 
 def assert_sudoku_by_region(board: list) -> bool:
 	row_length = len(board[0])
 	start, end = 0, 3
 
-	for t in range(0, 3):
-		for i in range(0, 7, 3):
+	step = 0
+	for i in gen_primes():
+		if row_length % i == 0:
+			step = i
+			break
 
+	for t in range(0, step):
+		# print()
+		for i in range(0, row_length - step + 1 if step % 2 != 0 else 0, step):
 			region = set()
-			for a in range(i, i + 3):
+			for a in range(i, i + step):
 				row = board[a][start: end]
 				for b in row:
 					region.add(b)
-
+			# print(region)
 			if len(region) != row_length:
 				# print('assert_sudoku_by_region')
 				return False
