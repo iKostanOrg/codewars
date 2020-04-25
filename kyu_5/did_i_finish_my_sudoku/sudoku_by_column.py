6da4f7215beac321145e3fdfed004d4346293346
Egor Kostan <igorkostan@gmail.com>
2020-04-24 19:09:57 -0700

    Update sudoku_by_column.py
    Found indentation with tabs instead of spaces

--- kyu_5/did_i_finish_my_sudoku/sudoku_by_column.py
+++ kyu_5/did_i_finish_my_sudoku/sudoku_by_column.py
@@ -4,13 +4,13 @@
 
 
 def assert_sudoku_by_column(board: list) -> bool:
-	row_length = len(board[0])
-	for i in range(0, row_length):
-		col = set()
-		for row in board:
-			col.add(row[i])
-		if len(col) != row_length:
-			print('assert_sudoku_by_column')
-			return False
+    row_length = len(board[0])
+    for i in range(0, row_length):
+        col = set()
+        for row in board:
+            col.add(row[i])
+        if len(col) != row_length:
+            print('assert_sudoku_by_column')
+            return False
 
-	return True
+    return True
