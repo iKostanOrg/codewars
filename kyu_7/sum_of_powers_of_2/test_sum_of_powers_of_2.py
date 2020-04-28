d49aecc4780cb7c6f8c471def49b067f500353b4
Egor Kostan <igorkostan@gmail.com>
2020-04-28 13:56:18 -0700

    Update test_sum_of_powers_of_2.py
    Variable name "n" doesn't conform to snake_case naming style

--- kyu_7/sum_of_powers_of_2/test_sum_of_powers_of_2.py
+++ kyu_7/sum_of_powers_of_2/test_sum_of_powers_of_2.py
@@ -45,112 +45,112 @@ class SumOfPowerOfTwoTestCase(unittest.TestCase):
                                         "<p></p>")
 
         with allure.step("Pass n = 1 and verify the output"):
-            n = 1
+            number = 1
             expected = [1]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 2 and verify the output"):
-            n = 2
+            number = 2
             expected = [2]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 4 and verify the output"):
-            n = 4
+            number = 4
             expected = [4]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 6 and verify the output"):
-            n = 6
+            number = 6
             expected = [2, 4]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 14 and verify the output"):
-            n = 14
+            number = 14
             expected = [2, 4, 8]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 32 and verify the output"):
-            n = 32
+            number = 32
             expected = [32]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 128 and verify the output"):
-            n = 128
+            number = 128
             expected = [128]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 512 and verify the output"):
-            n = 512
+            number = 512
             expected = [512]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 514 and verify the output"):
-            n = 514
+            number = 514
             expected = [2, 512]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 688 and verify the output"):
-            n = 688
+            number = 688
             expected = [16, 32, 128, 512]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 8197 and verify the output"):
-            n = 8197
+            number = 8197
             expected = [1, 4, 8192]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 1966 and verify the output"):
-            n = 1966
+            number = 1966
             expected = [2, 4, 8, 32, 128, 256, 512, 1024]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 134217736 and verify the output"):
-            n = 134217736
+            number = 134217736
             expected = [8, 134217728]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 140737488355330 and verify the output"):
-            n = 140737488355330
+            number = 140737488355330
             expected = [2, 140737488355328]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 35184372088896 and verify the output"):
-            n = 35184372088896
+            number = 35184372088896
             expected = [64, 35184372088832]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
 
         with allure.step("Pass n = 9007199254740991 and verify the output"):
-            n = 9007199254740991
+            number = 9007199254740991
             expected = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192,
                         16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152,
                         4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456,
@@ -160,5 +160,5 @@ class SumOfPowerOfTwoTestCase(unittest.TestCase):
                         35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312,
                         1125899906842624, 2251799813685248, 4503599627370496]
 
-            print_log(n=n, expected=expected)
-            self.assertEqual(powers(n), expected)
+            print_log(n=number, expected=expected)
+            self.assertEqual(powers(number), expected)
