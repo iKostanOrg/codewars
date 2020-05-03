cdf4a4260c8771c5d75e439b21a0ef7f2b3227f6
Egor Kostan <igorkostan@gmail.com>
2020-05-02 23:10:36 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,14 +45,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# The following example configures Sphinx to parse all files with
-# the extensions .md and .txt as Markdown:
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'restructuredtext',
-    '.md': 'markdown',
-}
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
