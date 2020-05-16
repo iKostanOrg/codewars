61a7df427b7e25fbdb887131208f3fa5ab3a006b
Egor Kostan <igorkostan@gmail.com>
2020-05-16 14:28:15 -0700

    Update test_increment_string.py

--- kyu_5/string_incrementer/test_increment_string.py
+++ kyu_5/string_incrementer/test_increment_string.py
@@ -38,7 +38,12 @@ class StringIncrementerTestCase(unittest.TestCase):
                                         '<img src="https://www.codewars.com/users/myFirstCode'
                                         '/badges/large">'
                                         '<h3>Test Description:</h3>'
-                                        "<p></p>")
+                                        "<p>"
+                                        "- If the string already ends with a number, the number "
+                                        "should be incremented by 1.<br/>"
+                                        "- If the string does not end with a number. the number 1 "
+                                        "should be appended to the new string."
+                                        "</p>")
 
         test_data = (
             ("foo", "foo1"),
