50a7a14e978334bbdc9524eae7a051fe849fcc99
Egor Kostan <igorkostan@gmail.com>
2020-05-25 15:48:04 -0700

    Update solution.py

--- kyu_3/make_spiral/solution.py
+++ kyu_3/make_spiral/solution.py
@@ -4,239 +4,239 @@
 
 
 DIRECTIONS = {
-	'right': 'down',
-	'down': 'left',
-	'left': 'up',
-	'up': 'right'
+    'right': 'down',
+    'down': 'left',
+    'left': 'up',
+    'up': 'right'
 }
 
 
 def right(spiral: list, coordinates: dict) -> bool:
-	"""
-    Move spiral right
-
-    :param coordinates: starting point
-    :param spiral: NxN spiral 2D array
-    :return: boolean 'done'
     """
-	done = True
+Move spiral right
 
-	while coordinates['col'] < len(spiral[coordinates['row']]):
+:param coordinates: starting point
+:param spiral: NxN spiral 2D array
+:return: boolean 'done'
+"""
+    done = True
 
-		row = coordinates['row']
-		col = coordinates['col']
+    while coordinates['col'] < len(spiral[coordinates['row']]):
 
-		if col == len(spiral[0]) - 1 and \
-				spiral[row][col] == 0:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif col + 1 == len(spiral[0]) - 1 and \
-				spiral[row][col + 1] == 0 and \
-				spiral[row][col] == 0:
-			spiral[row][col] = 1
-			coordinates['col'] += 1
-			done = False
-		elif col + 2 == len(spiral[0]) - 1 and \
-				spiral[row][col + 2] == 0 and \
-				spiral[row][col + 1] == 0 and \
-				spiral[row][col] == 0:
-			spiral[row][col] = 1
-			coordinates['col'] += 1
-			done = False
-		elif col + 2 == len(spiral[0]) - 1 and \
-				spiral[row][col + 2] == 1 and \
-				spiral[row][col + 1] == 0 and \
-				spiral[row][col] == 0:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif col + 2 < len(spiral[0]) - 1 and \
-				spiral[row][col + 2] == 1 and \
-				spiral[row][col + 1] == 0 and \
-				spiral[row][col] == 0 and \
-				col + 2 < len(spiral[0]) and \
-				spiral[row + 1][col] != 1:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif col + 2 < len(spiral[0]) - 1 and \
-				spiral[row][col + 2] == 0 and \
-				spiral[row][col + 1] == 0 and \
-				spiral[row][col] == 0 and \
-				col + 2 < len(spiral[0]):
-			spiral[row][col] = 1
-			coordinates['col'] += 1
-			done = False
-		else:
-			break
+        row = coordinates['row']
+        col = coordinates['col']
 
-	return done
+        if col == len(spiral[0]) - 1 and \
+                spiral[row][col] == 0:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif col + 1 == len(spiral[0]) - 1 and \
+                spiral[row][col + 1] == 0 and \
+                spiral[row][col] == 0:
+            spiral[row][col] = 1
+            coordinates['col'] += 1
+            done = False
+        elif col + 2 == len(spiral[0]) - 1 and \
+                spiral[row][col + 2] == 0 and \
+                spiral[row][col + 1] == 0 and \
+                spiral[row][col] == 0:
+            spiral[row][col] = 1
+            coordinates['col'] += 1
+            done = False
+        elif col + 2 == len(spiral[0]) - 1 and \
+                spiral[row][col + 2] == 1 and \
+                spiral[row][col + 1] == 0 and \
+                spiral[row][col] == 0:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif col + 2 < len(spiral[0]) - 1 and \
+                spiral[row][col + 2] == 1 and \
+                spiral[row][col + 1] == 0 and \
+                spiral[row][col] == 0 and \
+                col + 2 < len(spiral[0]) and \
+                spiral[row + 1][col] != 1:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif col + 2 < len(spiral[0]) - 1 and \
+                spiral[row][col + 2] == 0 and \
+                spiral[row][col + 1] == 0 and \
+                spiral[row][col] == 0 and \
+                col + 2 < len(spiral[0]):
+            spiral[row][col] = 1
+            coordinates['col'] += 1
+            done = False
+        else:
+            break
+
+    return done
 
 
 def down(spiral: list, coordinates: dict) -> bool:
-	"""
-    Move spiral down
-
-    :param coordinates: starting point
-    :param spiral: NxN spiral 2D array
-    :return: boolean 'done'
     """
-	done = True
+Move spiral down
 
-	while coordinates['row'] < len(spiral):
+:param coordinates: starting point
+:param spiral: NxN spiral 2D array
+:return: boolean 'done'
+"""
+    done = True
 
-		row = coordinates['row']
-		col = coordinates['col']
+    while coordinates['row'] < len(spiral):
 
-		if row == len(spiral) - 1 and spiral[row][col] == 0:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif row + 2 <= len(spiral) - 1 and \
-				spiral[row + 2][col] == 0 and \
-				spiral[row + 1][col] == 0:
-			spiral[row][col] = 1
-			coordinates['row'] += 1
-			done = False
-		elif row + 1 == len(spiral) - 1 and \
-				spiral[row + 1][col] == 0:
-			spiral[row][col] = 1
-			coordinates['row'] += 1
-			done = False
-		else:
-			break
+        row = coordinates['row']
+        col = coordinates['col']
 
-	return done
+        if row == len(spiral) - 1 and spiral[row][col] == 0:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif row + 2 <= len(spiral) - 1 and \
+                spiral[row + 2][col] == 0 and \
+                spiral[row + 1][col] == 0:
+            spiral[row][col] = 1
+            coordinates['row'] += 1
+            done = False
+        elif row + 1 == len(spiral) - 1 and \
+                spiral[row + 1][col] == 0:
+            spiral[row][col] = 1
+            coordinates['row'] += 1
+            done = False
+        else:
+            break
+
+    return done
 
 
 def left(spiral: list, coordinates: dict) -> bool:
-	"""
-    Move spiral left
-
-    :param coordinates: starting point
-    :param spiral: NxN spiral 2D array
-    :return: None
     """
-	done = True
+Move spiral left
 
-	while coordinates['col'] >= 0:
+:param coordinates: starting point
+:param spiral: NxN spiral 2D array
+:return: None
+"""
+    done = True
 
-		row = coordinates['row']
-		col = coordinates['col']
+    while coordinates['col'] >= 0:
 
-		if col == 0 and spiral[row][col] == 0:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif col - 2 >= 0 and \
-				spiral[row][col - 2] == 0 and \
-				spiral[row][col - 1] == 0:
-			spiral[row][col] = 1
-			coordinates['col'] -= 1
-			done = False
-		elif col - 2 >= 0 and \
-				spiral[row][col - 2] == 1 and \
-				spiral[row][col - 1] == 0 and \
-				spiral[row - 1][col] != 1:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif col - 2 == 0 and \
-				spiral[row][col - 2] == 1 and \
-				spiral[row][col - 1] == 0 and \
-				spiral[row][col] == 0 and \
-				spiral[row - 1][col] != 1:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif col - 1 == 0 and \
-				spiral[row][col - 1] == 0 and \
-				spiral[row][col] == 0:
-			spiral[row][col] = 1
-			coordinates['col'] -= 1
-			done = False
-		else:
-			break
+        row = coordinates['row']
+        col = coordinates['col']
 
-	return done
+        if col == 0 and spiral[row][col] == 0:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif col - 2 >= 0 and \
+                spiral[row][col - 2] == 0 and \
+                spiral[row][col - 1] == 0:
+            spiral[row][col] = 1
+            coordinates['col'] -= 1
+            done = False
+        elif col - 2 >= 0 and \
+                spiral[row][col - 2] == 1 and \
+                spiral[row][col - 1] == 0 and \
+                spiral[row - 1][col] != 1:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif col - 2 == 0 and \
+                spiral[row][col - 2] == 1 and \
+                spiral[row][col - 1] == 0 and \
+                spiral[row][col] == 0 and \
+                spiral[row - 1][col] != 1:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif col - 1 == 0 and \
+                spiral[row][col - 1] == 0 and \
+                spiral[row][col] == 0:
+            spiral[row][col] = 1
+            coordinates['col'] -= 1
+            done = False
+        else:
+            break
+
+    return done
 
 
 def up(spiral: list, coordinates: dict) -> bool:
-	"""
-    Move spiral up
-
-    :param coordinates: starting point
-    :param spiral: NxN spiral 2D array
-    :return: None
     """
-	done = True
+Move spiral up
 
-	while coordinates['row'] >= 0:
+:param coordinates: starting point
+:param spiral: NxN spiral 2D array
+:return: None
+"""
+    done = True
 
-		row = coordinates['row']
-		col = coordinates['col']
+    while coordinates['row'] >= 0:
 
-		if row == 0 and spiral[row][col] == 0:
-			spiral[row][col] = 1
-			done = False
-			break
-		elif row - 2 >= 0 and spiral[row - 2][col] == 0 and spiral[row - 1][col] == 0:
-			spiral[row][col] = 1
-			coordinates['row'] -= 1
-			done = False
-		elif row - 1 == 0 and spiral[row - 1][col] == 0:
-			spiral[row][col] = 1
-			coordinates['row'] -= 1
-			done = False
-		else:
-			break
+        row = coordinates['row']
+        col = coordinates['col']
 
-	return done
+        if row == 0 and spiral[row][col] == 0:
+            spiral[row][col] = 1
+            done = False
+            break
+        elif row - 2 >= 0 and spiral[row - 2][col] == 0 and spiral[row - 1][col] == 0:
+            spiral[row][col] = 1
+            coordinates['row'] -= 1
+            done = False
+        elif row - 1 == 0 and spiral[row - 1][col] == 0:
+            spiral[row][col] = 1
+            coordinates['row'] -= 1
+            done = False
+        else:
+            break
+
+    return done
 
 
 def set_initial_params(size: int) -> tuple:
-	"""
-    Set initial parameters: line, spiral, direction, coordinate, done
-
-    :param size:
-    :return:
     """
-	spiral: list = list()
-	for x in range(0, size):
-		line: list = [0] * size
-		spiral.append(line)
+Set initial parameters: line, spiral, direction, coordinate, done
 
-	direction: str = 'right'
-	coordinate: dict = {
-		'row': 0,
-		'col': 0
-	}
-	done = False
+:param size:
+:return:
+"""
+    spiral: list = list()
+    for x in range(0, size):
+        line: list = [0] * size
+        spiral.append(line)
 
-	return spiral, direction, coordinate, done
+    direction: str = 'right'
+    coordinate: dict = {
+        'row': 0,
+        'col': 0
+    }
+    done = False
+
+    return spiral, direction, coordinate, done
 
 
 def spiralize(size: int) -> list:
-	"""
-    Creates a NxN spiral 2D list with a given size
-
-    :param size: size of the 2D array
-    :return: NxN spiral 2D array
     """
-	spiral, direction, coordinates, done = set_initial_params(size)
+Creates a NxN spiral 2D list with a given size
 
-	while not done:
-		# 1 Moving spiral:
-		if direction == 'right':
-			done = right(spiral, coordinates)
-		elif direction == 'down':
-			done = down(spiral, coordinates)
-		elif direction == 'left':
-			done = left(spiral, coordinates)
-		elif direction == 'up':
-			done = up(spiral, coordinates)
-		# 2 Update direction:
-		direction = DIRECTIONS[direction]
+:param size: size of the 2D array
+:return: NxN spiral 2D array
+"""
+    spiral, direction, coordinates, done = set_initial_params(size)
 
-	return spiral
+    while not done:
+        # 1 Moving spiral:
+        if direction == 'right':
+            done = right(spiral, coordinates)
+        elif direction == 'down':
+            done = down(spiral, coordinates)
+        elif direction == 'left':
+            done = left(spiral, coordinates)
+        elif direction == 'up':
+            done = up(spiral, coordinates)
+        # 2 Update direction:
+        direction = DIRECTIONS[direction]
+
+    return spiral
