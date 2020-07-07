2c7f6805be837c93a19c2cf2ad0c4b162f59c0b2
Egor Kostan <igorkostan@gmail.com>
2020-07-07 15:57:32 -0700

    Next smaller number with the same digits

---
+++ kyu_4/next_smaller_number_with_the_same_digits/next_smaller.py
@@ -0,0 +1,67 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def next_smaller(n: int) -> int:
+    """
+    A function that takes a positive integer and returns the
+    next smaller positive integer containing the same digits.
+
+    If no smaller number can be composed using those digits, return -1
+    """
+    # 1
+    # Starting from the right, find the index of the first digit that
+    # has at least one smaller digit to its right. We'll call that digit X.
+    x_i: int = find_x(n)
+    #print('\nx: {}, index: {}'.format(str(n)[x_i], x_i))
+    if x_i < 0:
+        return x_i
+
+    # 2
+    # Then find the index of the largest digit that is to the
+    # right of X, and is smaller than X. We'll call that digit Y.
+    y_i: int = find_y(n, x_i)
+    #print('y: {}, index: {}'.format(str(n)[y_i], y_i))
+    if y_i < 0:
+        return y_i
+
+    # 3
+    # Swap X and Y. This makes the number smaller.
+    n_list: list = [int(i) for i in str(n)]
+    n_list[x_i], n_list[y_i] = n_list[y_i], n_list[x_i]
+
+    # 4
+    # Then sort all of the digits to the right of Y in descending order.
+    # This makes the number as big as possible, without making it bigger
+    # than the original.
+    result: int = int(''.join([str(i) for i in (n_list[:x_i + 1] + sorted(n_list[x_i + 1:], reverse=True))]))
+    return result if len(str(result)) == len(str(n)) else -1
+
+
+def find_x(n: int) -> int:
+    n_list: list = [int(i) for i in str(n)[::-1]]
+    for index, digit in enumerate(n_list):
+        if index - 1 >= 0 and n_list[index] > n_list[index - 1]:
+            return len(n_list) - index - 1
+    return -1
+
+
+def find_y(n: int, x_i: int) -> int:
+    n_list: list = [int(i) for i in str(n)]
+    comparable_x = {
+        'x': n_list[x_i],
+        'index': None,
+        'y': None
+    }
+
+    for index, y in enumerate(n_list[x_i + 1:]):
+        if comparable_x['x'] > y:
+            if not comparable_x['y']:
+                comparable_x['y'] = y
+                comparable_x['index'] = index + x_i + 1
+            else:
+                if comparable_x['y'] < y:
+                    comparable_x['y'] = y
+                    comparable_x['index'] = index + x_i + 1
+    return comparable_x['index'] if comparable_x['index'] else -1
---
+++ kyu_4/next_smaller_number_with_the_same_digits/test_next_smaller.py
@@ -0,0 +1,76 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+# ALGORITHMS NUMBERS STRINGS INTEGERS MATHEMATICS
+
+import allure
+import unittest
+from utils.log_func import print_log
+from kyu_4.next_smaller_number_with_the_same_digits.next_smaller import next_smaller
+
+
+@allure.epic("4 kyu")
+@allure.parent_suite('Competent')
+@allure.suite("Algorithms")
+@allure.sub_suite("Unit Tests")
+@allure.feature("String")
+@allure.story("Next smaller number with the same digits")
+@allure.tag('ALGORITHMS', 'NUMBERS', 'STRINGS', 'INTEGERS', 'MATHEMATICS')
+@allure.link(url='https://www.codewars.com/kata/5659c6d896bc135c4c00021e/train/python',
+             name='Source/Kata')
+class NextSmallerTestCase(unittest.TestCase):
+
+    def test_next_smaller(self):
+        """
+        Testing next_smaller function
+
+        You have to test a function that takes a positive integer number
+        and returns the next smaller number formed by the same digits:
+
+        12 ==> 21
+        513 ==> 531
+        2017 ==> 2071
+
+        If no smaller number can be composed using those digits, return -1
+        """
+
+        allure.dynamic.title("Testing next_smaller function")
+        allure.dynamic.severity(allure.severity_level.NORMAL)
+        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
+                                        '<img src="https://www.codewars.com/users/myFirstCode'
+                                        '/badges/large">'
+                                        '<h3>Test Description:</h3>'
+                                        "<p>Testing next_bigger function</p>"
+                                        "<p>You have to test a function that takes a positive integer "
+                                        "number and returns the next smaller number formed by the same digits:</p>"
+                                        "<p>12 ==> 21"
+                                        "513 ==> 531"
+                                        "2017 ==> 2071</p>"
+                                        "<p>If no smaller number can be composed using those digits, return -1</p>")
+
+        test_data = [
+            (1027, -1),
+            (1262347, 1247632),
+            (907, 790),
+            (531, 513),
+            (135, -1),
+            (2071, 2017),
+            (414, 144),
+            (123456798, 123456789),
+            (123456789, -1),
+            (1234567908, 1234567890),
+        ]
+
+        for n, expected in test_data:
+            actual_result = next_smaller(n)
+            print_log(n=n,
+                      expected=expected,
+                      actual_result=actual_result)
+
+            with allure.step("Enter an integer ({}), "
+                             "calculate the result ({}) and "
+                             "compare it with expected ({})".format(n,
+                                                                    actual_result,
+                                                                    expected)):
+                self.assertEqual(expected, actual_result)
