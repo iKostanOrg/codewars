a3ba99f79178c5640dc8e05f6f7cdf5637f3b196
Egor Kostan <igorkostan@gmail.com>
2024-08-14 20:29:06 -0700

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- kyu_8/alternating_case/test_alternating_case.py
+++ kyu_8/alternating_case/test_alternating_case.py
@@ -1,6 +1,9 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Testing for altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # FUNDAMENTALS
 
