d6e0956bc59ce3d1ea11157fff8378c294edf798
Egor Kostan <igorkostan@gmail.com>
2024-08-17 19:11:33 -0700

    Sphinx
    /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/latest/kyu_6/help_the_bookseller/stock_list.py:docstring of kyu_6.help_the_bookseller.stock_list.stock_list:3: ERROR: Unexpected indentation.
    
    /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/latest/kyu_6/help_the_bookseller/stock_list.py:docstring of kyu_6.help_the_bookseller.stock_list.stock_list:4: WARNING: Block quote ends without a blank line; unexpected unindent.

--- kyu_6/help_the_bookseller/stock_list.py
+++ kyu_6/help_the_bookseller/stock_list.py
@@ -1,21 +1,25 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Solution for -> Help the bookseller !
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 
 def stock_list(listOfArt: list, listOfCat: list) -> str:
     """
-    You will be given a stocklist (e.g. : L) and a
+    You will be given a stockist (e.g. : L) and a
     list of categories in capital letters e.g :
         M = {"A", "B", "C", "W"}
-    or
-        M = ["A", "B", "C", "W"] or ...
+        M = ["A", "B", "C", "W"]
 
     and your task is to find all the books of L with
     codes belonging to each category of M and to sum
     their quantity according to each category.
-    """
 
+    :param listOfArt: list
+    :param listOfCat: list
+    :return: string
+    """
     result = ''
 
     if not listOfArt:
