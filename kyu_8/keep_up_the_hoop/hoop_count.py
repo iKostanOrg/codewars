8bdf20dfb397667258afc72cf1483b9e7832f443
Egor Kostan <igorkostan@gmail.com>
2024-08-17 19:50:41 -0700

    Sphinx
    /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/latest/kyu_8/keep_up_the_hoop/test_hoop_count.py:docstring of kyu_8.keep_up_the_hoop.test_hoop_count.HoopCountTestCase.test_hoop_count_positive:11: WARNING: Bullet list ends without a blank line; unexpected unindent.

--- kyu_8/keep_up_the_hoop/hoop_count.py
+++ kyu_8/keep_up_the_hoop/hoop_count.py
@@ -1,14 +1,17 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Solution -> Keep up the hoop
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 
 def hoop_count(n) -> str:
     """
     A program where Alex can input (n) how many times the
     hoop goes round and it will return him an encouraging message
-    :param n:
-    :return:
+    
+    :param n: int
+    :return: str
     """
 
     if n < 10:
