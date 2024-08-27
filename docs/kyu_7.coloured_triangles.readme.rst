d9de166cbd1b7ef7660323ca91a8e3ca387bb49b
Egor Kostan <igorkostan@gmail.com>
2024-08-26 21:27:16 -0700

    coloured_triangles
    A coloured triangle is created from a row of colours, each of which is red, green or blue. Successive rows, each containing one fewer colour than the last, are generated by considering the two touching colours in the previous row. If these colours are identical, the same colour is used in the new row. If they are different, the missing colour is used in the new row. This is continued until the final row, with only a single colour, is generated.

---
+++ docs/kyu_7.coloured_triangles.readme.rst
@@ -0,0 +1,5 @@
+README
+======
+
+.. include:: ../kyu_7/coloured_triangles/README.md
+   :parser: myst_parser.sphinx_
---
+++ kyu_7/coloured_triangles/test_triangle.py
@@ -0,0 +1,83 @@
+"""
+Test for -> Coloured Triangles
+
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
+
+# Logic Strings Algorithms
+
+import unittest
+import allure
+from utils.log_func import print_log
+from kyu_7.coloured_triangles.solution_for_triangle import triangle
+
+
+@allure.epic('7 kyu')
+@allure.parent_suite('Beginner')
+@allure.suite("Data Structures")
+@allure.sub_suite("Unit Tests")
+@allure.feature("Lists")
+@allure.story('Coloured Triangles')
+@allure.tag('Logic',
+            'Strings',
+            'Algorithms')
+@allure.link(url='',
+             name='Source/Kata')
+class TriangleTestCase(unittest.TestCase):
+    """
+    Testing triangle function
+    """
+
+    def test_basic(self):
+        """
+        Basic test case
+        :return:
+        """
+
+        with allure.step("Enter test string and verify the output"):
+            test_data = [
+                ('GB', 'R'),
+                ('RRR', 'R'),
+                ('RGBG', 'B'),
+                ('RBRGBRB', 'G'),
+                ('RBRGBRBGGRRRBGBBBGG', 'G'),
+                ('B', 'B'),
+            ]
+
+            for string, expected in test_data:
+                print_log(string=string, expected=expected)
+                self.assertEqual(expected, triangle(string))
+
+    def test_random(self):
+        """
+        Advanced/random test case
+        :return:
+        """
+
+        with allure.step("Enter test string and verify the output"):
+            test_data = [
+                ('BGBGB', 'R'),
+                ('BBRRBRGBRRBRGRRBGGRRBBGBGGRGGB', 'G'),
+                ('RB', 'G'),
+                ('GRBGRGBBRBRGRRGGGGRBRBRGGRB', 'B'),
+                ('BBGRBGRRGGGRRBRBRBGRBGRRRBBBG', 'G'),
+                ('RRBBRRGBRBGBRBRGBGGRBBBBRGGRGB', 'R'),
+                ('GGBRGBBRBGRRGGGBGBGRGBGGRGRB', 'R'),
+                ('BRBBGGRGBGGGBGRBRGRGRRBBGBR', 'G'),
+                ('GBBRBRGGGGBRGGBBGGBGBRGRBGRGBB', 'G'),
+                ('RRRBRRGRRGBGBBRGRGRGRB', 'B'),
+                ('BRGGRBBBBGBRRRRBRBRRBGBGRBGB', 'B'),
+                ('RRBRBRBBBBBRBRRBBBGBBGBGGGRGR', 'G'),
+            ]
+
+            for string, expected in test_data:
+                result = triangle(string)
+                print_log(string=string,
+                          expected=expected,
+                          result=result)
+                self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
