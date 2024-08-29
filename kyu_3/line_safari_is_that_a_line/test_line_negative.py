0756db7bfabf85cf8e15befcefd3b74b8ed95330
Egor Kostan <igorkostan@gmail.com>
2024-08-28 19:50:02 -0700

    Update test_line_negative.py

--- kyu_3/line_safari_is_that_a_line/test_line_negative.py
+++ kyu_3/line_safari_is_that_a_line/test_line_negative.py
@@ -8,9 +8,9 @@ GitHub: https://github.com/ikostan
 
 # ALGORITHMS STRINGS
 
-import allure
 import unittest
 import pytest
+import allure
 from utils.log_func import print_log
 from kyu_3.line_safari_is_that_a_line.line_safari import line
 
