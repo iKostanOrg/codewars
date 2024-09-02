d3906bbbdab291ad8e82f27b812747e1f1c76886
Egor Kostan <igorkostan@gmail.com>
2024-09-01 19:48:41 -0700

    Update test_permutations.py
    kyu_4/sum_of_intervals/test_sum_of_intervals.py:1:0: R0801: Similar lines in 2 files
    ==kyu_4.permutations.test_permutations:[16:21]
    ==kyu_4.range_extraction.test_solution:[15:20]

--- kyu_4/permutations/test_permutations.py
+++ kyu_4/permutations/test_permutations.py
@@ -14,6 +14,7 @@ from utils.log_func import print_log
 from kyu_4.permutations.permutations import permutations
 
 
+# pylint: disable-msg=R0801
 @allure.epic("4 kyu")
 @allure.parent_suite('Competent')
 @allure.suite("Algorithms")
@@ -26,6 +27,7 @@ from kyu_4.permutations.permutations import permutations
 @allure.link(url='https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/python',
              name='Source/Kata')
 @pytest.mark.skip(reason="The solution is not ready")
+# pylint: enable-msg=R0801
 class PermutationsTestCase(unittest.TestCase):
 
     def test_permutations(self):
@@ -41,34 +43,34 @@ class PermutationsTestCase(unittest.TestCase):
 
         allure.dynamic.title("Testing permutations function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p>Testing permutations function</p>"
-                                        "<p>Test that permutations function creates all "
-                                        "permutations of an input string and "
-                                        "remove duplicates, if present. This means, you "
-                                        "have to shuffle all letters from the input in all "
-                                        "possible orders.</p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p>Testing permutations function</p>"
+            "<p>Test that permutations function creates all "
+            "permutations of an input string and "
+            "remove duplicates, if present. This means, you "
+            "have to shuffle all letters from the input in all "
+            "possible orders.</p>")
 
         test_data = [
-                ('a', ['a']),
-                ('ab', ['ab', 'ba']),
-                ('aabb', ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']),
-                ('abc', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']),
-                ('abcd', ['abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb',
-                          'bacd', 'badc', 'bcad', 'bcda', 'bdac', 'bdca',
-                          'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba',
-                          'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba']),
-                ('dcba', ['abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb',
-                          'bacd', 'badc', 'bcad', 'bcda', 'bdac', 'bdca',
-                          'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba',
-                          'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba'])
-            ]
+            ('a', ['a']),
+            ('ab', ['ab', 'ba']),
+            ('aabb', ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']),
+            ('abc', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']),
+            ('abcd', ['abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb',
+                      'bacd', 'badc', 'bcad', 'bcda', 'bdac', 'bdca',
+                      'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba',
+                      'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba']),
+            ('dcba', ['abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb',
+                      'bacd', 'badc', 'bcad', 'bcda', 'bdac', 'bdca',
+                      'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba',
+                      'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba'])
+        ]
         # pylint: disable-msg=R0801
         for string, expected in test_data:
-
             actual_result = sorted(permutations(string))
             print_log(string=string,
                       expected=expected,
