e8bfd1878bc1365bcdb3e40d918125ceaf5cf9a3
Egor Kostan <igorkostan@gmail.com>
2024-09-01 19:31:54 -0700

    Update test_format_duration.py
    kyu_4/human_readable_duration_format/test_format_duration.py:53:0: C0301: Line too long (109/100) (line-too-long)
    kyu_4/human_readable_duration_format/test_format_duration.py:54:0: C0301: Line too long (109/100) (line-too-long)
    kyu_4/human_readable_duration_format/test_format_duration.py:55:0: C0301: Line too long (102/100) (line-too-long)
    
    kyu_4/human_readable_duration_format/test_format_duration.py:75:29: C0209: Formatting a regular string which could be an f-string (consider-using-f-string)

--- kyu_4/human_readable_duration_format/test_format_duration.py
+++ kyu_4/human_readable_duration_format/test_format_duration.py
@@ -44,39 +44,38 @@ class FormatDurationTestCase(unittest.TestCase):
 
         allure.dynamic.title("Testing format_duration")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p>Test a function which formats a duration, given as a "
-                                        "number of seconds, in a human-friendly way.</p>"
-                                        "<p>The function must accept a non-negative integer. If it is zero, "
-                                        "it just returns \"now\". Otherwise, the duration is expressed as a "
-                                        "combination of years, days, hours, minutes and seconds.</p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p>Test a function which formats a duration, given as a "
+            "number of seconds, in a human-friendly way.</p>"
+            "<p>The function must accept a non-negative integer. If it is zero, "
+            "it just returns \"now\". Otherwise, the duration is expressed as a "
+            "combination of years, days, hours, minutes and seconds.</p>")
 
         test_data = [
-                (1, "1 second"),
-                (62, "1 minute and 2 seconds"),
-                (120, "2 minutes"),
-                (3600, "1 hour"),
-                (3662, "1 hour, 1 minute and 2 seconds"),
-                (15731080, '182 days, 1 hour, 44 minutes and 40 seconds'),
-                (132030240, '4 years, 68 days, 3 hours and 4 minutes'),
-                (205851834, '6 years, 192 days, 13 hours, 3 minutes and 54 seconds'),
-                (253374061, '8 years, 12 days, 13 hours, 41 minutes and 1 second'),
-                (242062374, '7 years, 246 days, 15 hours, 32 minutes and 54 seconds'),
-                (101956166, '3 years, 85 days, 1 hour, 9 minutes and 26 seconds'),
-                (33243586, '1 year, 19 days, 18 hours, 19 minutes and 46 seconds'),
-                (9041160, '104 days, 15 hours and 26 minutes'),
-            ]
+            (1, "1 second"),
+            (62, "1 minute and 2 seconds"),
+            (120, "2 minutes"),
+            (3600, "1 hour"),
+            (3662, "1 hour, 1 minute and 2 seconds"),
+            (15731080, '182 days, 1 hour, 44 minutes and 40 seconds'),
+            (132030240, '4 years, 68 days, 3 hours and 4 minutes'),
+            (205851834, '6 years, 192 days, 13 hours, 3 minutes and 54 seconds'),
+            (253374061, '8 years, 12 days, 13 hours, 41 minutes and 1 second'),
+            (242062374, '7 years, 246 days, 15 hours, 32 minutes and 54 seconds'),
+            (101956166, '3 years, 85 days, 1 hour, 9 minutes and 26 seconds'),
+            (33243586, '1 year, 19 days, 18 hours, 19 minutes and 46 seconds'),
+            (9041160, '104 days, 15 hours and 26 minutes'),
+        ]
 
         for seconds, expected in test_data:
             actual_result = format_duration(seconds)
-            with allure.step("Enter seconds ({}) and verify the "
-                             "expected output ({}) vs actual result ({})".format(seconds,
-                                                                                 expected,
-                                                                                 actual_result)):
-
+            with allure.step(f"Enter seconds ({seconds}) and verify the "
+                             f"expected output ({expected}) vs "
+                             f"actual result ({actual_result})"):
                 print_log(seconds=seconds,
                           expected=expected,
                           result=actual_result)
