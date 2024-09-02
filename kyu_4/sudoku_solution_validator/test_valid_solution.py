d2f77daf428873400eb6fad6467ec88ad86f16bc
Egor Kostan <igorkostan@gmail.com>
2024-09-01 19:12:21 -0700

    Update test_valid_solution.py

--- kyu_4/sudoku_solution_validator/test_valid_solution.py
+++ kyu_4/sudoku_solution_validator/test_valid_solution.py
@@ -49,14 +49,14 @@ class ValidSolutionTestCase(unittest.TestCase):
                                         '/badges/large">'
                                         '<h3>Test Description:</h3>'
                                         "<p>Test a function valid_solution()"
-                                        " that accepts a 2D array representing a Sudoku board,"
-                                        " and returns true if it is a valid solution, or false"
-                                        " otherwise. The cells of the sudoku board may also "
-                                        "contain 0's, which will represent empty cells. "
-                                        "Boards containing one or more zeroes are considered"
-                                        " to be invalid solutions.</p>"
-                                        "<p>The board is always 9 cells by 9 cells, and every "
-                                        "cell only contains integers from 0 to 9.</p>")
+                                        " that accepts a 2D array representing a Sudoku"
+                                        " board, and returns true if it is a valid solution,"
+                                        " or false otherwise. The cells of the sudoku board"
+                                        " may also contain 0's, which will represent empty"
+                                        " cells. Boards containing one or more zeroes are"
+                                        " considered to be invalid solutions.</p>"
+                                        "<p>The board is always 9 cells by 9 cells, and"
+                                        " every cell only contains integers from 0 to 9.</p>")
 
         test_data = [
             ([[5, 3, 4, 6, 7, 8, 9, 1, 2],
