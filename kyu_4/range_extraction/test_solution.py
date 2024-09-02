c1f4598984b7dcfe93049b6b3cf8718c63efb344
Egor Kostan <igorkostan@gmail.com>
2024-09-01 20:41:26 -0700

    Update test_solution.py
    kyu_4/sum_of_intervals/test_sum_of_intervals.py:1:0: R0801: Similar lines in 2 files
    ==kyu_4.range_extraction.test_solution:[38:44]
    ==kyu_4.the_greatest_warrior.test_warrior:[44:50]

--- kyu_4/range_extraction/test_solution.py
+++ kyu_4/range_extraction/test_solution.py
@@ -27,7 +27,6 @@ from kyu_4.range_extraction.solution import solution
             'STRINGS')
 @allure.link(url='https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/train/python',
              name='Source/Kata')
-# pylint: enable-msg=R0801
 class SolutionTestCase(unittest.TestCase):
 
     def test_solution(self):
@@ -46,7 +45,7 @@ class SolutionTestCase(unittest.TestCase):
             "<p>Test the solution so that it takes a list of integers "
             "in increasing order and returns a correctly formatted "
             "string in the range format.</p>")
-
+        # pylint: enable-msg=R0801
         test_data = [
             ([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9,
               10, 11, 14, 15, 17, 18, 19, 20],
