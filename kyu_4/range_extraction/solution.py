33991f902c8892b3cab72cdcca9ca554d27744f6
Egor Kostan <igorkostan@gmail.com>
2024-09-02 18:30:15 -0700

    Update solution.py
    Over-indented New
                        result += ',' + str(current[1])
    Severity: MinorFound in kyu_4/range_extraction/solution.py by pep8

--- kyu_4/range_extraction/solution.py
+++ kyu_4/range_extraction/solution.py
@@ -47,7 +47,7 @@ def solution(args: list) -> str:
             if abs(current[1] - current[0]) >= 2:
                 result += '-' + str(current[1])
             elif current[0] != current[1]:
-                    result += ',' + str(current[1])
+                result += ',' + str(current[1])
 
         if i == len(args) - 1 and current[-1] != a and current[2] is True:
             result += str(a)
