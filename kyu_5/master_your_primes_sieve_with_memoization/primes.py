c16ee16ae8721cc544b9c65a2b84339cc4939da2
Egor Kostan <igorkostan@gmail.com>
2024-09-02 21:40:23 -0700

    ************ Module kyu_5.master_your_primes_sieve_with_memoization.primes
    ************ Module kyu_5.master_your_primes_sieve_with_memoization.primes
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:9:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:18:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:19:0: W0311: Bad indentation. Found 2 spaces, expected 8 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:21:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:22:0: W0311: Bad indentation. Found 2 spaces, expected 8 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:23:0: W0311: Bad indentation. Found 3 spaces, expected 12 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:25:0: W0311: Bad indentation. Found 2 spaces, expected 8 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:26:0: W0311: Bad indentation. Found 3 spaces, expected 12 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:28:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)
    kyu_5/master_your_primes_sieve_with_memoization/primes.py:1:0: C0114: Missing module docstring (missing-module-docstring)

--- kyu_5/master_your_primes_sieve_with_memoization/primes.py
+++ kyu_5/master_your_primes_sieve_with_memoization/primes.py
@@ -1,28 +1,31 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Solution for -> Master your primes: sieve with memoization
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 primes = [2, 3, 5, 7]
 
 
-def is_prime(n):
-	"""
-	A function that checks if a given number n is a prime looping
-	through it and, possibly, expanding the array/list of known
-	primes only if/when necessary (ie: as soon as you check for a
-	potential prime which is greater than a given threshold for each n, stop).
-	:param n:
-	:return:
-	"""
+def is_prime(n: int) -> bool:
+    """
+    A function that checks if a given number n is a prime looping
+    through it and, possibly, expanding the array/list of known
+    primes only if/when necessary (ie: as soon as you check for a
+    potential prime which is greater than a given threshold for
+    each n, stop}.
+    :param n: int
+    :return: bool
+    """
 
-	if n < 2:
-		return False
+    if n < 2:
+        return False
 
-	for i in range(3, int(n ** 0.5) + 1, 2):
-		if is_prime(i) and i not in primes:
-			primes.append(i)
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if is_prime(i) and i not in primes:
+            primes.append(i)
 
-		if n % i == 0:
-			return False
+        if n % i == 0:
+            return False
 
-	return True
+    return True
