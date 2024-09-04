d0b5ec79d4e1ce4ea6f68411ab6503ce9d2565dd
Egor Kostan <igorkostan@gmail.com>
2024-09-03 18:13:54 -0700

    kyu_5/first_non_repeating_character/__init__.py:1:0: R0801: Similar lines in 2 files
    kyu_5/first_non_repeating_character/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_5.extract_the_domain_name_from_url.test_domain_name:[42:47]
    ==kyu_5.sports_league_table_ranking.test_compute_ranks:[39:44]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>' (duplicate-code)

--- kyu_5/extract_the_domain_name_from_url/test_domain_name.py
+++ kyu_5/extract_the_domain_name_from_url/test_domain_name.py
@@ -1,11 +1,9 @@
 """
 Assert that 'domain_name' function
 returns domain name from given URL string.
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
 """
-
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 # FUNDAMENTALS PARSING ALGORITHMS STRINGS REGULAR EXPRESSIONS
 # DECLARATIVE PROGRAMMING ADVANCED LANGUAGE FEATURES
@@ -22,8 +20,12 @@ from kyu_5.extract_the_domain_name_from_url.extract_domain_from_url import domai
 @allure.sub_suite("Unit Tests")
 @allure.feature("String")
 @allure.story('Extract the domain name from a URL')
-@allure.tag('FUNDAMENTALS', 'PARSING', 'ALGORITHMS', 'STRINGS',
-            'REGULAR EXPRESSIONS', 'DECLARATIVE PROGRAMMING',
+@allure.tag('FUNDAMENTALS',
+            'PARSING',
+            'ALGORITHMS',
+            'STRINGS',
+            'REGULAR EXPRESSIONS',
+            'DECLARATIVE PROGRAMMING',
             'ADVANCED LANGUAGE FEATURES')
 @allure.link(url='https://www.codewars.com/kata/514a024011ea4fb54200004b/train/python',
              name='Source/Kata')
@@ -36,18 +38,19 @@ class DomainNameTestCase(unittest.TestCase):
         """
         Assert that 'domain_name' function
         returns domain name from given URL string.
-
         :return:
         """
+        # pylint: disable-msg=R0801
         allure.dynamic.title("Testing domain_name function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p>Assert that 'domain_name' function "
-                                        "returns domain name from given URL string.</p>")
-
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p>Assert that 'domain_name' function "
+            "returns domain name from given URL string.</p>")
+        # pylint: enable-msg=R0801
         test_data = (
             ("http://google.com", "google"),
             ("http://google.co.jp", "google"),
@@ -58,5 +61,7 @@ class DomainNameTestCase(unittest.TestCase):
         for url, expected in test_data:
             with allure.step("Enter test string and verify the output"):
                 actual = domain_name(url)
-                print_log(url=url, expected=expected, actual=actual)
+                print_log(url=url,
+                          expected=expected,
+                          actual=actual)
                 self.assertEqual(expected, actual)
