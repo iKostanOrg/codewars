fd8f7b84bfe507e673e182f1ae8073bcc2fd54c9
Egor Kostan <igorkostan@gmail.com>
2024-09-04 21:03:53 -0700

    Update hashtag_generator.py

--- kyu_5/the_hashtag_generator/hashtag_generator.py
+++ kyu_5/the_hashtag_generator/hashtag_generator.py
@@ -12,7 +12,7 @@ def generate_hashtag(s: str):
     1. It must start with a hashtag (#).
     2. All words must have their first letter capitalized.
     3. If the final result is longer than 140 chars it must return false.
-    4. If the input or the result is an empty string it must return false.
+    4. If the input or t he result is an empty string it must return false.
 
     :param s: str
     :return:
@@ -20,8 +20,8 @@ def generate_hashtag(s: str):
     if len(s.strip()) < 1:
         return False
 
-    result = ('#' +
-              (''.join([string.capitalize() for string in s.split(' ')
-                        if string.strip() != ''])))
+    result: str = ''.join(string.capitalize() for string in s.split(' ')
+                          if string.strip() != '')
+    result = '#' + result
 
     return False if len(result) > 140 else result
