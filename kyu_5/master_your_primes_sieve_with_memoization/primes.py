038a35f7aeb02c0664ab1a6d00be33bbc9570743
Egor Kostan <igorkostan@gmail.com>
2024-09-04 20:56:20 -0700

    Update primes.py
    codefactor.io
    / CodeFactor
    kyu_5/master_your_primes_sieve_with_memoization/primes.py#L10
    Argument name "n" doesn't conform to snake_case naming style (invalid-name)

--- kyu_5/master_your_primes_sieve_with_memoization/primes.py
+++ kyu_5/master_your_primes_sieve_with_memoization/primes.py
@@ -5,26 +5,26 @@ GitHub: https://github.com/ikostan
 """
 
 
-def is_prime(n: int) -> bool:
+def is_prime(digit: int) -> bool:
     """
-    A function that checks if a given number n is a prime looping
+    A function that checks if a given number 'digit' is a prime looping
     through it and, possibly, expanding the array/list of known
     primes only if/when necessary (ie: as soon as you check for a
     potential prime which is greater than a given threshold for
-    each n, stop}.
-    :param n: int
+    each 'digit', stop}.
+    :param digit: int
     :return: bool
     """
     primes = [2, 3, 5, 7]
 
-    if n < 2:
+    if digit < 2:
         return False
 
-    for i in range(3, int(n ** 0.5) + 1, 2):
+    for i in range(3, int(digit ** 0.5) + 1, 2):
         if is_prime(i) and i not in primes:
             primes.append(i)
 
-        if n % i == 0:
+        if digit % i == 0:
             return False
 
     return True
