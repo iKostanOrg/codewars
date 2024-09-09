1a6e61d22a31d148a78f9843b34d63925d9930d9
Egor Kostan <igorkostan@gmail.com>
2024-09-08 20:50:06 -0700

    kyu_5 docs fixes
    /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/449/kyu_5/find_the_smallest/solution.py:docstring of kyu_5.find_the_smallest.solution.smallest:6: WARNING: Enumerated list ends without a blank line; unexpected unindent.
    /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/449/kyu_5/sports_league_table_ranking/test_compute_ranks.py:docstring of kyu_5.sports_league_table_ranking.test_compute_ranks.ComputeRanksTestCase.test_something:10: WARNING: Bullet list ends without a blank line; unexpected unindent.
    /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/449/kyu_5/sports_league_table_ranking/test_compute_ranks.py:docstring of kyu_5.sports_league_table_ranking.test_compute_ranks.ComputeRanksTestCase.test_something:12: WARNING: Block quote ends without a blank line; unexpected unindent.

--- kyu_5/sports_league_table_ranking/test_compute_ranks.py
+++ kyu_5/sports_league_table_ranking/test_compute_ranks.py
@@ -36,10 +36,11 @@ class ComputeRanksTestCase(unittest.TestCase):
         the order of the teams in your league. You use the following
         criteria to arrange the teams:
 
-            - Points
-            - Scoring differential (the difference between goals
-            scored and those conceded)
-            - Goals scored
+        - Points
+        - Scoring differential (the difference between goals
+        scored and those conceded)
+        - Goals scored
+
         :return:
         """
         # pylint: disable-msg=R0801
@@ -60,6 +61,7 @@ class ComputeRanksTestCase(unittest.TestCase):
             "<li>- Scoring differential (the difference between goals "
             "scored and those conceded)</li>"
             "<li>- Goals scored</li></ul>")
+        
         # pylint: enable-msg=R0801
         test_data = [
             (6,
