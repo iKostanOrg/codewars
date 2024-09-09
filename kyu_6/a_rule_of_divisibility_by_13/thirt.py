2211eb3f02a3a47aeb08ccb1901c7f2cae8b9d40
Egor Kostan <igorkostan@gmail.com>
2024-09-08 21:41:37 -0700

    A Rule of Divisibility by 13

--- kyu_6/a_rule_of_divisibility_by_13/thirt.py
+++ kyu_6/a_rule_of_divisibility_by_13/thirt.py
@@ -1,20 +1,22 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Solution for -> A Rule of Divisibility by 13
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
+
+REMAINDERS = (1, 10, 9, 12, 3, 4)
 
 
 def thirt(n: int) -> int:
     """
     The function which processes this sequence of operations
     on an integer n (>=0). `thirt` will return the stationary number.
-    :param n:
-    :return:
+    :param n: int
+    :return: int
     """
-    REMAINDERS = (1, 10, 9, 12, 3, 4)
-
     while True:
-        i = 0
-        temp = 0
+        i: int = 0
+        temp: int = 0
         t = str(n)[::-1]
         for s in t:
             temp += int(s) * REMAINDERS[i]
@@ -27,4 +29,3 @@ def thirt(n: int) -> int:
             return temp
         else:
             n = temp
-
