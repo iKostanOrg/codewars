9f24fb1269577c8a79aa3efdd91979f9d3624477
Egor Kostan <igorkostan@gmail.com>
2024-09-09 20:43:41 -0700

    Update epidemic_test_data.py
    ************* Module kyu_6.disease_spread.epidemic_test_data
    kyu_6/disease_spread/epidemic_test_data.py:86:15: C0209: Formatting a regular string which could be an f-string (consider-using-f-string)

--- kyu_6/disease_spread/epidemic_test_data.py
+++ kyu_6/disease_spread/epidemic_test_data.py
@@ -83,14 +83,9 @@ class EpidemicTestData:
         return self.__expected
 
     def __repr__(self):
-        return 'tm: {}, n: {}, s0: {}, i0: {}, ' \
-               'b: {}, a: {}, expected: {}'.format(self.tm,
-                                                   self.n,
-                                                   self.s0,
-                                                   self.i0,
-                                                   self.b,
-                                                   self.a,
-                                                   self.expected)
+        return (f'tm: {self.tm}, n: {self.n}, s0: {self.s0}, '
+                f'i0: {self.i0}, b: {self.b}, a: {self.a}, '
+                f'expected: {self.expected}')
 
     def __eq__(self, other):
         """
