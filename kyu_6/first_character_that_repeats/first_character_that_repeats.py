da2e7bbe7ba16eeba1c98546d61a561122803bb7
Egor Kostan <igorkostan@gmail.com>
2024-09-09 19:37:43 -0700

    ************* Module kyu_6.first_character_that_repeats
    ************* Module kyu_6.first_character_that_repeats.first_character_that_repeats
    kyu_6/first_character_that_repeats/first_character_that_repeats.py:1:0: C0114: Missing module docstring (missing-module-docstring)
    ************* Module kyu_6.first_character_that_repeats.test_first_character_that_repeats
    kyu_6/first_character_that_repeats/test_first_character_that_repeats.py:1:0: C0114: Missing module docstring (missing-module-docstring)

--- kyu_6/first_character_that_repeats/first_character_that_repeats.py
+++ kyu_6/first_character_that_repeats/first_character_that_repeats.py
@@ -1,17 +1,18 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Solution for -> First character that repeats
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 
-def first_dup(s):
+def first_dup(s: str) -> (str, None):
     """
     Find the first character that repeats
     in a String and return that character.
-    :param s:
-    :return:
+    :param s: str
+    :return: str, None
     """
     for char in s:
         if s.count(char) > 1:
             return char
-
     return None
--- kyu_6/first_character_that_repeats/test_first_character_that_repeats.py
+++ kyu_6/first_character_that_repeats/test_first_character_that_repeats.py
@@ -1,6 +1,8 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test for -> First character that repeats
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # ALGORITHMS
 
@@ -16,12 +18,12 @@ from kyu_6.first_character_that_repeats.first_character_that_repeats import firs
 @allure.sub_suite("Unit Tests")
 @allure.feature("String")
 @allure.story('First character that repeats')
-@allure.tag()
-@allure.link(url='',
+@allure.tag('ALGORITHMS')
+@allure.link(url='https://www.codewars.com/kata/54f9f4d7c41722304e000bbb',
              name='Source/Kata')
 class FirstDupTestCase(unittest.TestCase):
     """
-    Testing first_dup function
+    Testing first_dup function.
 
     Find the first character that repeats
     in a String and return that character.
@@ -29,24 +31,24 @@ class FirstDupTestCase(unittest.TestCase):
 
     def test_first_dup_none(self):
         """
-        Test string with no duplicate chars
+        Test string with no duplicate chars.
         :return:
         """
 
         allure.dynamic.title("String with no duplicate chars")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Pass string with no repeating chars"):
             string = 'like'
             expected = None
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
         with allure.step("Pass string with no repeating chars"):
@@ -54,29 +56,28 @@ class FirstDupTestCase(unittest.TestCase):
             expected = None
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
     def test_first_dup_mixed(self):
         """
-        Test string with mixed type of chars
+        Test string with mixed type of chars.
         :return:
         """
 
         allure.dynamic.title("String with mixed type of chars")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Input consist of mixed type of chars"):
             string = '1a2b3a3c'
             expected = 'a'
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
         with allure.step("Input consist of alphabet chars and spaces"):
@@ -84,71 +85,69 @@ class FirstDupTestCase(unittest.TestCase):
             expected = 'o'
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
     def test_first_alpha_only(self):
         """
-        Test string with alphabet chars only
+        Test string with alphabet chars only.
         :return:
         """
         allure.dynamic.title("String with alphabet chars only")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Input consist of alphabet chars only"):
             string = 'tweet'
             expected = 't'
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
     def test_first_space(self):
         """
-        Repeating char is a space
+        Repeating char is a space.
         :return:
         """
         allure.dynamic.title("String alphabet chars and spaces")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Input consist of alphabet chars and spaces"):
             string = 'Ode to Joy'
             expected = ' '
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
     def test_first_no_alpha(self):
         """
-        Test string with no alphabet chars
+        Test string with no alphabet chars.
         :return:
         """
-
         allure.dynamic.title("String with no alphabet chars")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Pass string with digits only"):
             string = '123123'
             expected = '1'
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
 
         with allure.step("Pass string with special chars only"):
@@ -156,5 +155,4 @@ class FirstDupTestCase(unittest.TestCase):
             expected = '!'
 
             print_log(string=string, expected=expected)
-
             self.assertEqual(first_dup(string), expected)
