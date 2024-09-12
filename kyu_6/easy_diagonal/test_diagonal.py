c430758e9fac75c7d08f3b7d2fbcfbd67fbef3d0
Egor Kostan <igorkostan@gmail.com>
2024-09-11 19:48:36 -0700

    ==kyu_6.easy_diagonal.test_diagonal:[31:36]
    kyu_6/unique_in_order/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_6.easy_diagonal.test_diagonal:[31:36]
    ==kyu_6.format_string_of_names.test_namelist:[42:47]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>' (duplicate-code)

--- kyu_6/easy_diagonal/test_diagonal.py
+++ kyu_6/easy_diagonal/test_diagonal.py
@@ -1,9 +1,11 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test for -> Easy Diagonal
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
-import allure
 import unittest
+import allure
 from utils.log_func import print_log
 from kyu_6.easy_diagonal.diagonal import diagonal
 
@@ -15,32 +17,35 @@ from kyu_6.easy_diagonal.diagonal import diagonal
 @allure.feature("Algorithms")
 @allure.story('Easy Diagonal')
 @allure.tag("FUNDAMENTALS", "ALGORITHMS")
-@allure.link(url='https://www.codewars.com/kata/'
-                 '559b8e46fa060b2c6a0000bf/train/python',
-             name='Source/Kata')
+@allure.link(
+    url='https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf/train/python',
+    name='Source/Kata')
 class EasyDiagonalTestCase(unittest.TestCase):
     """
     Testing easy_diagonal function
     """
+
     def test_easy_diagonal(self):
         """
         Testing easy_diagonal function
         :param self:
         :return:
         """
+        # pylint: disable-msg=R0801
         allure.dynamic.title("Testing easy_diagonal function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p>"
-                                        "We want to calculate the sum of the binomial coefficients on a "
-                                        "given diagonal. The sum on diagonal 0 is 8 (we'll write it S(7, 0), "
-                                        "7 is the number of the line where we start, 0 is the number of the diagonal). "
-                                        "In the same way S(7, 1) is 28, S(7, 2) is 56."
-                                        "</p>")
-
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p>"
+            "We want to calculate the sum of the binomial coefficients "
+            "on a given diagonal. The sum on diagonal 0 is 8 (we'll "
+            "write it S(7, 0), 7 is the number of the line where we start,"
+            " 0 is the number of the diagonal). In the same way S(7, 1) "
+            "is 28, S(7, 2) is 56.</p>")
+        # pylint: enable-msg=R0801
         test_data = (
             (7, 0, 8),
             (7, 1, 28),
@@ -59,8 +64,9 @@ class EasyDiagonalTestCase(unittest.TestCase):
             expected = td[2]
             result = diagonal(n, p)
             print_log(n=n, p=p, expected=expected, result=result)
-            with allure.step("Enter n: {}, "
-                             "p: {} and assert the "
-                             "expected: {} vs "
-                             "result: {}".format(n, p, expected, result)):
+
+            with allure.step(f"Enter n: {n}, "
+                             f"p: {p} and assert the "
+                             f"expected: {expected} vs "
+                             f"result: {result}"):
                 self.assertEqual(expected, result)
