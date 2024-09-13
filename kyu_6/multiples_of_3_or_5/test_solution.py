37c57f106db2210e916184064111b59ed379949d
Egor Kostan <igorkostan@gmail.com>
2024-09-12 19:24:09 -0700

    ==kyu_6.multiples_of_3_or_5.test_solution:[42:47]
    kyu_6/unique_in_order/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_6.multiples_of_3_or_5.test_solution:[42:47]
    ==kyu_6.who_likes_it.test_likes_function:[36:41]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>' (duplicate-code)

--- kyu_6/multiples_of_3_or_5/test_solution.py
+++ kyu_6/multiples_of_3_or_5/test_solution.py
@@ -1,6 +1,8 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test for -> Multiples of 3 or 5
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # ALGORITHMS MATHEMATICS NUMBERS
 
@@ -16,9 +18,12 @@ from kyu_6.multiples_of_3_or_5.solution import solution
 @allure.sub_suite("Unit Tests")
 @allure.feature("Algorithms")
 @allure.story('Multiples of 3 or 5')
-@allure.tag('ALGORITHMS', 'MATHEMATICS', 'NUMBERS')
-@allure.link(url='',
-             name='Source/Kata')
+@allure.tag('ALGORITHMS',
+            'MATHEMATICS',
+            'NUMBERS')
+@allure.link(
+    url='https://www.codewars.com/kata/514b92a657cdc65150000006/train/python',
+    name='Source/Kata')
 class SolutionTestCase(unittest.TestCase):
     """
     Testing solution function
@@ -38,17 +43,27 @@ class SolutionTestCase(unittest.TestCase):
         only count it once.
         :return:
         """
-
+        # pylint: disable-msg=R0801
         allure.dynamic.title("Testing the 'solution' function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
+        # pylint: enable-msg=R0801
+        test_data: tuple = (
+            (4, 3, 'Should return 3 for n=4'),
+            (200, 9168, 'Should return 9168 for n=200'),
+            (-1, 0, 'Should return 0 for n=-1'),
+            (1291, 388935, 'Random test')
+        )
 
-        with allure.step("Pass test data and verify the output"):
-            number = 10
-            expected = 23
-            print_log(expected=expected, number=number)
-            self.assertEqual(expected, solution(number))
+        for td in test_data:
+            number, expected, msg = td
+            with allure.step(
+                    f"Pass test data ({number}) and verify the output"):
+                
+                print_log(expected=expected, number=number, msg=msg)
+                self.assertEqual(expected, solution(number), msg)
