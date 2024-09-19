df75afa44433b0149be5f7135cc85724e523124b
Egor Kostan <igorkostan@gmail.com>
2024-09-18 20:08:38 -0700

    ==kyu_7.password_validator
    kyu_7/formatting_decimal_places_1/two_decimal_places.py:1:0: R0801: Similar lines in 2 files
    ==kyu_7.password_validator.test_password:[10:15]
    ==kyu_7.pull_your_words_together_man.test_sentencify:[12:17]
    @allure.epic('7 kyu')
    @allure.parent_suite('Beginner')
    @allure.suite("Data Structures")
    @allure.sub_suite("Unit Tests")
    @allure.feature("String") (duplicate-code)

--- kyu_7/password_validator/test_password.py
+++ kyu_7/password_validator/test_password.py
@@ -1,22 +1,33 @@
-# Created by Egor Kostan.
-# GitHub: https://github.com/ikostan
-# FUNDAMENTALS REGULAR EXPRESSIONS DECLARATIVE PROGRAMMING ADVANCED LANGUAGE FEATURES STRINGS
+"""
+Test for -> Password validator
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
+
+# FUNDAMENTALS REGULAR EXPRESSIONS DECLARATIVE PROGRAMMING
+# ADVANCED LANGUAGE FEATURES STRINGS
 
 import unittest
 import allure
 from utils.log_func import print_log
 from kyu_7.password_validator.password import password
 
-
+# pylint: disable-msg=R0801
 @allure.epic('7 kyu')
 @allure.parent_suite('Beginner')
 @allure.suite("Data Structures")
 @allure.sub_suite("Unit Tests")
 @allure.feature("String")
 @allure.story('Password validator')
-@allure.tag()
-@allure.link(url='',
-             name='Source/Kata')
+@allure.tag('FUNDAMENTALS',
+            'REGULAR EXPRESSIONS',
+            'DECLARATIVE PROGRAMMING',
+            'ADVANCED LANGUAGE FEATURES',
+            'STRINGS')
+@allure.link(
+    url='https://www.codewars.com/kata/56a921fa8c5167d8e7000053/train/python',
+    name='Source/Kata')
+# pylint: enable-msg=R0801
 class PasswordTestCase(unittest.TestCase):
     """
     Testing password function
@@ -27,14 +38,14 @@ class PasswordTestCase(unittest.TestCase):
         Testing password function with various test inputs
         :return:
         """
-
         allure.dynamic.title("Testing password function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Enter test string and verify the result"):
             test_data = [
