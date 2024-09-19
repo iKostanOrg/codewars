b149a539c16f343f0e7dbf96bef32117f5f77250
Egor Kostan <igorkostan@gmail.com>
2024-09-18 20:21:48 -0700

    ************* Module kyu_7.help_bob_count_letters_and_digits
    ************* Module kyu_7.help_bob_count_letters_and_digits.test_count_letters_and_digits
    kyu_7/help_bob_count_letters_and_digits/test_count_letters_and_digits.py:10:0: C0301: Line too long (101/100) (line-too-long)
    kyu_7/help_bob_count_letters_and_digits/test_count_letters_and_digits.py:1:0: C0114: Missing module docstring (missing-module-docstring)
    kyu_7/help_bob_count_letters_and_digits/test_count_letters_and_digits.py:26:4: C0116: Missing function or method docstring (missing-function-docstring)
    kyu_7/help_bob_count_letters_and_digits/test_count_letters_and_digits.py:46:29: C0209: Formatting a regular string which could be an f-string (consider-using-f-string)

--- kyu_7/help_bob_count_letters_and_digits/test_count_letters_and_digits.py
+++ kyu_7/help_bob_count_letters_and_digits/test_count_letters_and_digits.py
@@ -1,6 +1,8 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test for -> Help Bob count letters and digits.
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # FUNDAMENTALS STRINGS
 
@@ -16,37 +18,40 @@ from kyu_7.help_bob_count_letters_and_digits.count_letters_and_digits import cou
 @allure.sub_suite('Unit Tests')
 @allure.feature('String')
 @allure.story('Help Bob count letters and digits.')
-@allure.tag('FUNDAMENTALS', 'STRINGS')
-@allure.link(url='https://www.codewars.com/kata/5738f5ea9545204cec000155/train/python',
-             name='Source/Kata')
+@allure.tag('FUNDAMENTALS',
+            'STRINGS')
+@allure.link(
+    url='https://www.codewars.com/kata/5738f5ea9545204cec000155/train/python',
+    name='Source/Kata')
 class CalculateTestCase(unittest.TestCase):
     """
     Testing count_letters_and_digits function
     """
+
     def test_calculate(self):
         allure.dynamic.title("Testing count_letters_and_digits function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p>Test a method that can determine how many letters "
-                                        "and digits are in a given string.</p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p>Test a method that can determine how many letters "
+            "and digits are in a given string.</p>")
 
         test_data = [
             ('n!!ice!!123', 7),
             ('de?=?=tttes!!t', 8),
             ('', 0),
             ('!@#$%^&`~.', 0),
-            ('u_n_d_e_r__S_C_O_R_E', 10),
-        ]
+            ('u_n_d_e_r__S_C_O_R_E', 10)]
 
         for s, expected in test_data:
-            actual_result = count_letters_and_digits(s)
-            with allure.step("Enter string ({}) and verify the "
-                             "expected output ({}) vs actual result ({})".format(s,
-                                                                                 expected,
-                                                                                 actual_result)):
+            actual_result: int = count_letters_and_digits(s)
+            with allure.step(f"Enter string ({s}) and verify the "
+                             f"expected output ({expected}) vs "
+                             f"actual result ({actual_result})"):
+
                 print_log(s=s,
                           expected=expected,
                           result=actual_result)
