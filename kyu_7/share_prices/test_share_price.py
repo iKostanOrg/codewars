bc12083d0a364aba0fc839b3de0aeb8aaa70ed8b
Egor Kostan <igorkostan@gmail.com>
2024-09-18 20:30:16 -0700

    ==kyu_7.share_prices
    kyu_7/easy_line/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_7.share_prices.test_share_price:[12:17]
    ==kyu_7.sum_of_triangular_numbers.test_sum_triangular_numbers:[12:17]
    @allure.epic('7 kyu')
    @allure.parent_suite('Beginner')
    @allure.suite("Algorithms")
    @allure.sub_suite("Unit Tests")
    @allure.feature("Math") (duplicate-code)

--- kyu_7/share_prices/test_share_price.py
+++ kyu_7/share_prices/test_share_price.py
@@ -1,6 +1,8 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test for -> Share prices
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # ALGORITHMS FORMATTING ARITHMETIC MATHEMATICS NUMBERS
 
@@ -10,15 +12,22 @@ from utils.log_func import print_log
 from kyu_7.share_prices.share_price import share_price
 
 
+# pylint: disable-msg=R0801
 @allure.epic('7 kyu')
 @allure.parent_suite('Beginner')
 @allure.suite("Algorithms")
 @allure.sub_suite("Unit Tests")
 @allure.feature("Math")
 @allure.story('Share prices')
-@allure.tag()
-@allure.link(url='',
-             name='Source/Kata')
+@allure.tag('ALGORITHMS',
+            'FORMATTING',
+            'ARITHMETIC',
+            'MATHEMATICS',
+            'NUMBERS')
+@allure.link(
+    url='https://www.codewars.com/kata/5603a4dd3d96ef798f000068/train/python',
+    name='Source/Kata')
+# pylint: enable-msg=R0801
 class SharePriceTestCase(unittest.TestCase):
     """
     Testing share_price function
@@ -30,18 +39,17 @@ class SharePriceTestCase(unittest.TestCase):
         with multiple test inputs
         :return:
         """
-
         allure.dynamic.title("Testing share_price function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step("Enter invested, changes "
                          "and verify the output"):
-
             test_data = [
                 (100, [], '100.00'),
                 (100, [-50, 50], '75.00'),
@@ -51,7 +59,6 @@ class SharePriceTestCase(unittest.TestCase):
             ]
 
             for invested, changes, expected in test_data:
-
                 print_log(invested=invested,
                           changes=changes,
                           expected=False)
