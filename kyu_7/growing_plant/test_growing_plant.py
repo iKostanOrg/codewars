d9871093cc7594e990ee88ed36a3dc2b9f3f5eba
Egor Kostan <igorkostan@gmail.com>
2024-09-19 19:57:53 -0700

    ==kyu_7.growing_plant
    yu_7/easy_line/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_7.growing_plant.test_growing_plant:[12:17]
    ==kyu_7.sum_of_powers_of_2.test_sum_of_powers_of_2:[14:19]
    @allure.epic('7 kyu')
    @allure.parent_suite('Beginner')
    @allure.suite("Algorithms")
    @allure.sub_suite("Unit Tests")
    @allure.feature("Calculation") (duplicate-code)

--- kyu_7/growing_plant/test_growing_plant.py
+++ kyu_7/growing_plant/test_growing_plant.py
@@ -1,24 +1,28 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test -> Simple Fun #74: Growing Plant
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # ALGORITHMS
 
-import allure
 import unittest
+import allure
 from utils.log_func import print_log
 from kyu_7.growing_plant.growing_plant import growing_plant
 
-
+# pylint: disable-msg=R0801
 @allure.epic('7 kyu')
 @allure.parent_suite('Beginner')
 @allure.suite("Algorithms")
 @allure.sub_suite("Unit Tests")
 @allure.feature("Calculation")
 @allure.story('Simple Fun #74: Growing Plant')
-@allure.tag()
-@allure.link(url='',
-             name='Source/Kata')
+@allure.tag('ALGORITHMS')
+@allure.link(
+    url='https://www.codewars.com/kata/58941fec8afa3618c9000184/train/python',
+    name='Source/Kata')
+# pylint: enable-msg=R0801
 class GrowingPlantTestCase(unittest.TestCase):
     """
     Testing growing_plant function
@@ -66,34 +70,33 @@ class GrowingPlantTestCase(unittest.TestCase):
         reach/pass desiredHeight (including the last day in the
         total count).
         """
-
         allure.dynamic.title('Testing growing_plant function')
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
 
         with allure.step('Enter upSpeed, downSpeed and '
                          'desiredHeight and verify the output'):
-
-            test_data = [
+            test_data: tuple = (
                 ((100, 10, 910), 10),
                 ((10, 9, 4), 1),
                 ((5, 2, 0), 1),
                 ((5, 2, 5), 1),
-                ((5, 2, 6), 2),
-            ]
+                ((5, 2, 6), 2))
 
             for input_data, expected in test_data:
-
-                upSpeed = input_data[0]
-                downSpeed = input_data[1]
-                desiredHeight = input_data[2]
+                up_speed: int = input_data[0]
+                down_speed: int = input_data[1]
+                desired_height: int = input_data[2]
 
                 print_log(input_data=input_data,
                           expected=expected)
 
                 self.assertEqual(expected,
-                                 growing_plant(upSpeed, downSpeed, desiredHeight))
+                                 growing_plant(up_speed,
+                                               down_speed,
+                                               desired_height))
