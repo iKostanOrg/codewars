2226880c9379cfeb95ae8c3c575120785a01c0ed
Egor Kostan <igorkostan@gmail.com>
2024-09-23 20:26:35 -0700

    ==kyu_7.share_prices.test_share_price:[42:48]
    kyu_7/easy_line/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_7.share_prices.test_share_price:[42:48]
    ==kyu_7.sum_of_odd_numbers.test_row_sum_odd_numbers:[39:45]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html(
                '<h3>Codewars badge:</h3>'
                '<img src="https://www.codewars.com/users/myFirstCode'
                '/badges/large">'
                '<h3>Test Description:</h3>' (duplicate-code)

--- kyu_7/share_prices/test_share_price.py
+++ kyu_7/share_prices/test_share_price.py
@@ -39,6 +39,7 @@ class SharePriceTestCase(unittest.TestCase):
         with multiple test inputs
         :return:
         """
+        # pylint: disable-msg=R0801
         allure.dynamic.title("Testing share_price function")
         allure.dynamic.severity(allure.severity_level.NORMAL)
         allure.dynamic.description_html(
@@ -47,7 +48,7 @@ class SharePriceTestCase(unittest.TestCase):
             '/badges/large">'
             '<h3>Test Description:</h3>'
             "<p></p>")
-
+        # pylint: enable-msg=R0801
         with allure.step("Enter invested, changes "
                          "and verify the output"):
             test_data: tuple = (
