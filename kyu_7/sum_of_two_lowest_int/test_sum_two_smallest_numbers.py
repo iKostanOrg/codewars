306778454b44bf217e77460254e55135f065e7c3
Egor Kostan <igorkostan@gmail.com>
2024-09-26 18:33:03 -0700

    ==kyu_7.sum_of_two_lowest_int
    kyu_7/easy_line/__init__.py:1:0: R0801: Similar lines in 2 files
    ==kyu_7.sum_of_two_lowest_int.test_sum_two_smallest_numbers:[30:37]
    ==kyu_7.vaporcode.test_vaporcode:[33:40]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>'
                                            "<p></p>")
     (duplicate-code)

--- kyu_7/sum_of_two_lowest_int/test_sum_two_smallest_numbers.py
+++ kyu_7/sum_of_two_lowest_int/test_sum_two_smallest_numbers.py
@@ -1,10 +1,12 @@
+"""
+Test for -> Sum of two lowest positive integers
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
+
 import unittest
 import allure
 from kyu_7.sum_of_two_lowest_int.sum_two_smallest_int import sum_two_smallest_numbers
-
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 # FUNDAMENTALS, ARRAYS
 
@@ -15,9 +17,11 @@ from kyu_7.sum_of_two_lowest_int.sum_two_smallest_int import sum_two_smallest_nu
 @allure.sub_suite("Unit Tests")
 @allure.feature("Lists")
 @allure.story('Sum of two lowest positive integers')
-@allure.tag()
-@allure.link(url='',
-             name='Source/Kata')
+@allure.tag('FUNDAMENTALS',
+            'ARRAYS')
+@allure.link(
+    url='https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python',
+    name='Source/Kata')
 class SumTwoSmallestNumbersTestCase(unittest.TestCase):
 
     def test_sum_two_smallest_numbers(self):
@@ -27,15 +31,18 @@ class SumTwoSmallestNumbersTestCase(unittest.TestCase):
         the two lowest positive numbers
         :return:
         """
-        allure.dynamic.title("Two smallest numbers in the start of the list")
+        # pylint: disable-msg=R0801
+        allure.dynamic.title(
+            "Two smallest numbers in the start of the list")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
-
-        with allure.step(""):
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
+        # pylint: enable-msg=R0801
+        with allure.step("Simple test"):
             self.assertEqual(
                 sum_two_smallest_numbers([5, 8, 12, 18, 22]),
                 13)
