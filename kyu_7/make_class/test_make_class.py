617889bb49f1ecb110242dfe2f8fdaba36a55b99
Egor Kostan <igorkostan@gmail.com>
2024-09-27 21:06:07 -0700

    Update test_make_class.py
    ************* Module kyu_7.make_class.test_make_class
    kyu_7/make_class/test_make_class.py:73:37: E1101: Instance of 'Class' has no 'name' member (no-member)
    kyu_7/make_class/test_make_class.py:74:40: E1101: Instance of 'Class' has no 'species' member (no-member)
    kyu_7/make_class/test_make_class.py:75:36: E1101: Instance of 'Class' has no 'age' member (no-member)
    kyu_7/make_class/test_make_class.py:76:39: E1101: Instance of 'Class' has no 'health' member (no-member)
    kyu_7/make_class/test_make_class.py:77:39: E1101: Instance of 'Class' has no 'weight' member (no-member)
    kyu_7/make_class/test_make_class.py:78:38: E1101: Instance of 'Class' has no 'color' member (no-member)

--- kyu_7/make_class/test_make_class.py
+++ kyu_7/make_class/test_make_class.py
@@ -63,12 +63,12 @@ class MakeClassTestCase(unittest.TestCase):
                           weight="50lb",
                           color="brown")
 
-            dog2 = Animel("Bob",
-                          "Dog",
-                          5,
-                          "good",
-                          "50lb",
-                          "brown")
+            dog2 = Animel(name="Bob",
+                          species="Dog",
+                          age=5,
+                          health="good",
+                          weight="50lb",
+                          color="brown")
 
             test_data = ((dog1.name, dog2.name),
                          (dog1.species, dog2.species),
