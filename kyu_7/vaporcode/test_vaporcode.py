3908505e8c4c361133034a311456cef58d60d5fb
Egor Kostan <igorkostan@gmail.com>
2024-09-28 20:54:35 -0700

    Update test_vaporcode.py

--- kyu_7/vaporcode/test_vaporcode.py
+++ kyu_7/vaporcode/test_vaporcode.py
@@ -46,31 +46,31 @@ class VaporcodeTestCase(unittest.TestCase):
         # pylint: enable-msg=R0801
         with allure.step("Enter string with spaces"
                          " and verify the output"):
-            s = "Lets go to the movies"
-            expected = "L  E  T  S  G  O  T  O  " \
-                       "T  H  E  M  O  V  I  E  S"
-            print_log(s=s, expected=expected)
-            self.assertEqual(vaporcode(s), expected)
+            string: str = "Lets go to the movies"
+            expected: str = "L  E  T  S  G  O  T  O  " \
+                            "T  H  E  M  O  V  I  E  S"
+            print_log(s=string, expected=expected)
+            self.assertEqual(vaporcode(string), expected)
 
         with allure.step("Enter string with special "
                          "chars and verify the output"):
-            s = "Why isn't my code working?"
-            expected = "W  H  Y  I  S  N  '  T  M  Y  " \
-                       "C  O  D  E  W  O  R  K  I  N  G  ?"
-            print_log(s=s, expected=expected)
+            string: str = "Why isn't my code working?"
+            expected: str = "W  H  Y  I  S  N  '  T  M  Y  " \
+                            "C  O  D  E  W  O  R  K  I  N  G  ?"
+            print_log(s=string, expected=expected)
             self.assertEqual(vaporcode(s), expected)
 
         with allure.step("Enter crazy string and verify the output"):
-            s = " ; FUV! qd vz  Xy-b pM.!:F  lEqRLY,p RGS:;Rh Z "
-            expected = ";  F  U  V  !  Q  D  V  Z  X  Y  -  B  P  " \
-                       "M  .  !  :  F  L  E  Q  R  L  Y  ,  P  R  G  " \
-                       "S  :  ;  R  H  Z"
-            print_log(s=s, expected=expected)
-            self.assertEqual(vaporcode(s), expected)
+            string: str = " ; FUV! qd vz  Xy-b pM.!:F  lEqRLY,p RGS:;Rh Z "
+            expected: str = ";  F  U  V  !  Q  D  V  Z  X  Y  -  B  P  " \
+                            "M  .  !  :  F  L  E  Q  R  L  Y  ,  P  R  G  " \
+                            "S  :  ;  R  H  Z"
+            print_log(s=string, expected=expected)
+            self.assertEqual(vaporcode(string), expected)
 
         with allure.step("Enter string with chars"
                          "only and verify the output"):
-            s = "blah"
-            expected = "B  L  A  H"
-            print_log(s=s, expected=expected)
-            self.assertEqual(vaporcode(s), expected)
+            string: str = "blah"
+            expected: str = "B  L  A  H"
+            print_log(s=string, expected=expected)
+            self.assertEqual(vaporcode(string), expected)
