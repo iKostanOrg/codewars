4f59198fc79fce4a7e941ba8321aefb16256c216
Egor Kostan <igorkostan@gmail.com>
2024-09-28 21:59:27 -0700

    # Rail Fence Cipher: Encoding and Decoding

--- kyu_3/rail_fence_cipher_encoding_and_decoding/test_decoding.py
+++ kyu_3/rail_fence_cipher_encoding_and_decoding/test_decoding.py
@@ -27,7 +27,7 @@ from kyu_3.rail_fence_cipher_encoding_and_decoding.encoding_and_decoding \
             'SECURITY',
             'STRINGS')
 @allure.link(
-    url='https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/python',
+    url='https://www.codewars.com/kata/58c5577d61aefcf3ff000081',
     name='Source/Kata')
 # pylint: enable-msg=R0801
 class DecodingTestCase(unittest.TestCase):
@@ -39,7 +39,6 @@ class DecodingTestCase(unittest.TestCase):
         """
         Testing Decoding functionality
         """
-
         allure.dynamic.title("Testing Decoding functionality")
         allure.dynamic.severity(allure.severity_level.NORMAL)
         allure.dynamic.description_html(
@@ -50,7 +49,7 @@ class DecodingTestCase(unittest.TestCase):
             "<p>Verify cipher function. This \"decode\" is used "
             "to decode a string.</p>")
 
-        test_data = (
+        test_data: tuple = (
             ("H !e,Wdloollr", 4, "Hello, World!"),
             ("WECRLTEERDSOEEFEAOCAIVDEN", 3, "WEAREDISCOVEREDFLEEATONCE"),
             ("", 3, ""),
--- kyu_3/rail_fence_cipher_encoding_and_decoding/test_encoding.py
+++ kyu_3/rail_fence_cipher_encoding_and_decoding/test_encoding.py
@@ -27,7 +27,7 @@ from kyu_3.rail_fence_cipher_encoding_and_decoding.encoding_and_decoding \
             'SECURITY',
             'STRINGS')
 @allure.link(
-    url='https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/python',
+    url='https://www.codewars.com/kata/58c5577d61aefcf3ff000081',
     name='Source/Kata')
 # pylint: enable-msg=R0801
 class EncodingTestCase(unittest.TestCase):
@@ -51,7 +51,7 @@ class EncodingTestCase(unittest.TestCase):
             "a string by placing each character successively in a "
             "diagonal along a set of \"rails\". </p>")
         # pylint: enable-msg=R0801
-        test_data = (
+        test_data: tuple = (
             ("WEAREDISCOVEREDFLEEATONCE", 3, "WECRLTEERDSOEEFEAOCAIVDEN"),
             ("Hello, World!", 3, "Hoo!el,Wrdl l"),
             ("Hello, World!", 4, "H !e,Wdloollr"),
