6d439fdf97198410221e265d8fcd8098b0f5fa42
Egor Kostan <igorkostan@gmail.com>
2024-09-28 20:11:37 -0700

    ************* Module kyu_7.fun_with_lists_length
    ************* Module kyu_7.fun_with_lists_length.node
    kyu_7/fun_with_lists_length/node.py:12:29: W0622: Redefining built-in 'next' (redefined-builtin)
    kyu_7/fun_with_lists_length/node.py:8:0: R0903: Too few public methods (0/2) (too-few-public-methods)

--- kyu_7/fun_with_lists_length/node.py
+++ kyu_7/fun_with_lists_length/node.py
@@ -9,6 +9,40 @@ class Node:
     """
     The linked list
     """
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
+    def __init__(self, data, n_next=None):
+        self._data = data
+        self._next = n_next
+
+    @property
+    def next(self):
+        """
+        Get next
+        :return:
+        """
+        return self._next
+
+    @next.setter
+    def next(self, n_next=None) -> None:
+        """
+        Get next
+        :param n_next:
+        :return:
+        """
+        self._next = n_next
+
+    @property
+    def data(self):
+        """
+        Get data
+        :return:
+        """
+        return self._data
+
+    @data.setter
+    def data(self, data) -> None:
+        """
+        Get data
+        :param data:
+        :return:
+        """
+        self._data = data
