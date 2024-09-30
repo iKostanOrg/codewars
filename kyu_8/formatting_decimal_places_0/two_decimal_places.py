ff772fef3a84ffb84435b8cd376029eac899b922
Egor Kostan <igorkostan@gmail.com>
2024-09-29 21:00:12 -0700

    ==kyu_8.formatting_decimal_places_0.
    kyu_8/well_of_ideas_easy_version/test_well_of_ideas_easy_version.py:1:0: R0801: Similar lines in 2 files
    ==kyu_8.formatting_decimal_places_0.test_two_decimal_places:[43:50]
    ==kyu_8.set_alarm.test_set_alarm:[45:52]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>'
                                            "<p></p>")
     (duplicate-code)

--- kyu_8/formatting_decimal_places_0/two_decimal_places.py
+++ kyu_8/formatting_decimal_places_0/two_decimal_places.py
@@ -1,16 +1,18 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Solution for -> Formatting decimal places #0
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 
-def two_decimal_places(n):
+def two_decimal_places(n: float) -> float:
     """
     Each number should be formatted that it is
     rounded to two decimal places. You don't
     need to check whether the input is a valid
     number because only valid numbers are used
     in the tests.
-    :param n:
-    :return:
+    :param n: float
+    :return: float
     """
-    return float('{:.2f}'.format(n))
+    return float(f'{n:.2f}')
