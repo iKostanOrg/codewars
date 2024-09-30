33f08d4bb5cec2e5f84649e7c98d66170360881b
Egor Kostan <igorkostan@gmail.com>
2024-09-29 20:25:07 -0700

    Update counting_sheep.py
    Check warning on line 20 in kyu_8/counting_sheep/counting_sheep.py
    
    @codacy-production
    codacy-production
    / Codacy Static Code Analysis
    kyu_8/counting_sheep/counting_sheep.py#L20
    
    Bad indentation. Found 1 spaces, expected 4

--- kyu_8/counting_sheep/counting_sheep.py
+++ kyu_8/counting_sheep/counting_sheep.py
@@ -6,16 +6,16 @@ GitHub: https://github.com/ikostan
 
 
 def count_sheeps(array_of_sheeps: list) -> int:
-	"""
-	Consider an array of sheep where some sheep
-	may be missing from their place. We need a
-	function that counts the number of sheep
-	present in the array (true means present).
+    """
+    Consider an array of sheep where some sheep
+    may be missing from their place. We need a
+    function that counts the number of sheep
+    present in the array (true means present).
 
-	Hint: Don't forget to check for bad values
-	like null/undefined
-	:param array_of_sheeps:
-	:return:
-	"""
-	return 0 if array_of_sheeps is None \
-		else array_of_sheeps.count(True)
+    Hint: Don't forget to check for bad values
+    like null/undefined
+    :param array_of_sheeps:
+    :return:
+    """
+    return 0 if array_of_sheeps is None \
+        else array_of_sheeps.count(True)
