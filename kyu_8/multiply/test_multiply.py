392e6807598b6385381e07248671fb928bfc8324
Egor Kostan <igorkostan@gmail.com>
2024-09-29 20:38:52 -0700

    ==kyu_8.multiply
    kyu_8/well_of_ideas_easy_version/test_well_of_ideas_easy_version.py:1:0: R0801: Similar lines in 2 files
    ==kyu_8.multiply.test_multiply:[34:41]
    ==kyu_8.set_alarm.test_set_alarm:[45:52]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>'
                                            "<p></p>")
     (duplicate-code)

--- kyu_8/multiply/test_multiply.py
+++ kyu_8/multiply/test_multiply.py
@@ -1,6 +1,8 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+"""
+Test for -> Multiply
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
 
 # FUNDAMENTALS, INTRODUCTION
 
@@ -10,15 +12,19 @@ from utils.log_func import print_log
 from kyu_8.multiply.multiply import multiply
 
 
+# pylint: disable-msg=R0801
 @allure.epic('8 kyu')
 @allure.parent_suite('Beginner')
 @allure.suite("Math")
 @allure.sub_suite("Unit Tests")
 @allure.feature("Multiplication")
 @allure.story('Multiply')
-@allure.tag('FUNDAMENTALS', 'INTRODUCTION')
-@allure.link(url='https://www.codewars.com/kata/50654ddff44f800200000004/train/python',
-             name='Source/Kata')
+@allure.tag('FUNDAMENTALS',
+            'INTRODUCTION')
+@allure.link(
+    url='https://www.codewars.com/kata/50654ddff44f800200000004',
+    name='Source/Kata')
+# pylint: enable-msg=R0801
 class MultiplyTestCase(unittest.TestCase):
     """
     Testing multiply function
@@ -30,21 +36,20 @@ class MultiplyTestCase(unittest.TestCase):
         returns correct result
         :return:
         """
-
+        # pylint: disable-msg=R0801
         allure.dynamic.title("'multiply' function verification")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
-
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
+        # pylint: enable-msg=R0801
         with allure.step("Assert (a * b) result"):
-            a = 1
-            b = 2
-            expected = a * b
+            a: int = 1
+            b: int = 2
+            expected: int = a * b
 
             print_log(a=a, b=b, expected=expected)
-
-            self.assertEqual(expected,
-                             multiply(a, b))
+            self.assertEqual(expected, multiply(a, b))
