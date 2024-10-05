2eed9e02b565910e9cab347b2b5bba4b05859e90
Egor Kostan <igorkostan@gmail.com>
2024-10-04 21:03:59 -0700

    Update test_hoop_count.py

--- kyu_8/keep_up_the_hoop/test_hoop_count.py
+++ kyu_8/keep_up_the_hoop/test_hoop_count.py
@@ -39,11 +39,9 @@ class HoopCountTestCase(unittest.TestCase):
         Write a program where Alex can input (n) how many times
         the hoop goes round and it will return him an encouraging message
 
-        - If Alex gets 10 or more hoops, return the string "Great,
-        now move on to tricks".
-        
-        - If he doesn't get 10 hoops, return the string "Keep at it
-        until you get it".
+        - 10 or more hoops, return "Great, now move on to tricks".
+
+        - Not 10 hoops, return "Keep at it until you get it".
 
         :return:
         """
