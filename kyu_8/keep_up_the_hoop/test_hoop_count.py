513df9feb3aac49e355471457dc1d5e3157024bf
Egor Kostan <igorkostan@gmail.com>
2024-10-04 20:53:53 -0700

    ==kyu_8.keep_up_the_hoop
    kyu_8/well_of_ideas_easy_version/test_well_of_ideas_easy_version.py:1:0: R0801: Similar lines in 2 files
    ==kyu_8.keep_up_the_hoop.test_hoop_count:[61:68]
    ==kyu_8.remove_first_and_last_character.test_remove_char:[38:45]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>'
                                            "<p></p>")
     (duplicate-code)
    kyu_8/well_of_ideas_easy_version/test_well_of_ideas_easy_version.py:1:0: R0801: Similar lines in 2 files
    ==kyu_8.keep_up_the_hoop.test_hoop_count:[45:52]
    ==kyu_8.will_there_be_enough_space.test_enough:[40:47]
            allure.dynamic.severity(allure.severity_level.NORMAL)
            allure.dynamic.description_html('<h3>Codewars badge:</h3>'
                                            '<img src="https://www.codewars.com/users/myFirstCode'
                                            '/badges/large">'
                                            '<h3>Test Description:</h3>'
                                            "<p></p>")
     (duplicate-code)

--- kyu_8/keep_up_the_hoop/test_hoop_count.py
+++ kyu_8/keep_up_the_hoop/test_hoop_count.py
@@ -6,12 +6,13 @@ GitHub: https://github.com/ikostan
 
 # FUNDAMENTALS
 
-import allure
 import unittest
+import allure
 from utils.log_func import print_log
 from kyu_8.keep_up_the_hoop.hoop_count import hoop_count
 
 
+# pylint: disable=R0801
 @allure.epic('8 kyu')
 @allure.parent_suite('Beginner')
 @allure.suite("Fundamentals")
@@ -19,8 +20,10 @@ from kyu_8.keep_up_the_hoop.hoop_count import hoop_count
 @allure.feature("Conditions")
 @allure.story('Keep up the hoop')
 @allure.tag('FUNDAMENTALS')
-@allure.link(url='https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/python',
-             name='Source/Kata')
+@allure.link(
+    url='https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145',
+    name='Source/Kata')
+# pylint: enable=R0801
 class HoopCountTestCase(unittest.TestCase):
     """
     Testing hoop_count function
@@ -28,7 +31,7 @@ class HoopCountTestCase(unittest.TestCase):
 
     def test_hoop_count_positive(self):
         """
-        Testing hoop_count function
+        Testing hoop_count function (positive)
 
         Alex just got a new hula hoop, he loves it but feels
         discouraged because his little brother is better than him
@@ -36,38 +39,48 @@ class HoopCountTestCase(unittest.TestCase):
         Write a program where Alex can input (n) how many times
         the hoop goes round and it will return him an encouraging message
 
-        - If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
-        - If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+        - If Alex gets 10 or more hoops, return the string "Great,
+        now move on to tricks".
+        - If he doesn't get 10 hoops, return the string "Keep at it
+        until you get it".
 
         :return:
         """
-
-        allure.dynamic.title("Testing hoop_count function (positive test case)")
+        # pylint: disable=R0801
+        allure.dynamic.title(
+            "Testing hoop_count function (positive test case)")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
-
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
+        # pylint: enable=R0801
         with allure.step("Enter n and verify the result"):
-            n = 11
-            expected = "Great, now move on to tricks"
+            n: int = 11
+            expected: str = "Great, now move on to tricks"
             print_log(n=n, expected=expected)
             self.assertEqual(hoop_count(n), expected)
 
     def test_hoop_count_negative(self):
-
-        allure.dynamic.title("Testing hoop_count function (negative test case)")
+        """
+        Testing hoop_count function (negative)
+        :return:
+        """
+        # pylint: disable=R0801
+        allure.dynamic.title(
+            "Testing hoop_count function (negative test case)")
         allure.dynamic.severity(allure.severity_level.NORMAL)
-        allure.dynamic.description_html('<h3>Codewars badge:</h3>'
-                                        '<img src="https://www.codewars.com/users/myFirstCode'
-                                        '/badges/large">'
-                                        '<h3>Test Description:</h3>'
-                                        "<p></p>")
-
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
+        # pylint: enable=R0801
         with allure.step("Enter n and verify the result"):
-            n = 3
-            expected = "Keep at it until you get it"
+            n: int = 3
+            expected: str = "Keep at it until you get it"
             print_log(n=n, expected=expected)
             self.assertEqual(hoop_count(n), expected)
