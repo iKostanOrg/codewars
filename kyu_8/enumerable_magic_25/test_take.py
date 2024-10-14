5c2204ed6e8a8ada68a85e591d58b4465a0d9baa
Egor Kostan <igorkostan@gmail.com>
2024-10-13 19:01:45 -0700

    Update test_take.py

--- kyu_8/enumerable_magic_25/test_take.py
+++ kyu_8/enumerable_magic_25/test_take.py
@@ -42,9 +42,10 @@ class TakeTestCase(unittest.TestCase):
             '<img src="https://www.codewars.com/users/myFirstCode'
             '/badges/large">'
             '<h3>Test Description:</h3>'
-            "<p>Create a method take that accepts a list/array and a number n, "
-            "and returns a list/array array of the first n elements from the "
-            "list/array. If you need help, here's a reference: "
+            "<p>Create a method take that accepts a list/array and a"
+            " number n, and returns a list/array array of the first "
+            "n elements from the list/array. If you need help, here's"
+            " a reference: "
             "https://docs.python.org/3/library/stdtypes"
             ".html#sequence-types-list-tuple-range</p>")
         # pylint: enable=R0801
