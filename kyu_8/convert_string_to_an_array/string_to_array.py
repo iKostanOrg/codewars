e0d72b4c21b1b1b21aa0c7a78918d1cdd92b12bf
Egor Kostan <igorkostan@gmail.com>
2024-10-13 18:38:53 -0700

    Update string_to_array.py
    ************ Module kyu_8.convert_string_to_an_array.string_to_array
    kyu_8/convert_string_to_an_array/string_to_array.py:9:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)
    kyu_8/convert_string_to_an_array/string_to_array.py:15:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)

--- kyu_8/convert_string_to_an_array/string_to_array.py
+++ kyu_8/convert_string_to_an_array/string_to_array.py
@@ -6,10 +6,10 @@ GitHub: https://github.com/ikostan
 
 
 def string_to_array(s: str) -> list:
-	"""
-	A function to split a string and
-	convert it into an array of words
-	:param s: str
-	:return: list
-	"""
-	return s.split(' ')
+    """
+    A function to split a string and
+    convert it into an array of words
+    :param s: str
+    :return: list
+    """
+    return s.split(' ')
