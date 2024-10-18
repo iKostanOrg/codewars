87285082301fd0c8afe09dad6373e2fc28a0ae52
Egor Kostan <igorkostan@gmail.com>
2024-10-17 20:09:46 -0700

    Update test_odd_row.py

--- kyu_6/row_of_the_odd_triangle/test_odd_row.py
+++ kyu_6/row_of_the_odd_triangle/test_odd_row.py
@@ -67,10 +67,11 @@ class OddRowTestCase(unittest.TestCase):
                     f"Enter the triangle's row ({n}) and verify the "
                     f"expected output ({expected}) vs "
                     f"actual result ({actual_result})"):
-
+                # pylint: disable=R0801
                 print_log(n=n,
                           expected=expected,
                           result=actual_result)
 
                 self.assertEqual(expected,
                                  actual_result)
+                # pylint: disable=R0801
