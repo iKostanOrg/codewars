5624d4d72e1c2bd818561dc5700527a9ea020802
Egor Kostan <igorkostan@gmail.com>
2024-10-28 19:32:34 -0700

    Update test_first_non_consecutive.py
    kyu_8/find_the_first_non_consecutive_number/test_first_non_consecutive.py:78:0: C0325: Unnecessary parens after 'with' keyword (superfluous-parens)

--- kyu_8/find_the_first_non_consecutive_number/test_first_non_consecutive.py
+++ kyu_8/find_the_first_non_consecutive_number/test_first_non_consecutive.py
@@ -75,7 +75,7 @@ class FirstNonConsecutiveTestCase(unittest.TestCase):
             '<h3>Test Description:</h3>'
             "<p></p>")
         # pylint: enable=R0801
-        with (allure.step("Pass a large list with no non consecutive numbers")):
+        with allure.step("Pass a large list with no non consecutive numbers"):
             lst: list = [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
                          47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
                          58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
