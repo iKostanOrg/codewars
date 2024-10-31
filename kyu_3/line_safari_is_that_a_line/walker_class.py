21fac45bd6dec09e0333f9ee35991bf4595b99dd
Egor Kostan <igorkostan@gmail.com>
2024-10-30 20:16:36 -0700

    Update walker_class.py

--- kyu_3/line_safari_is_that_a_line/walker_class.py
+++ kyu_3/line_safari_is_that_a_line/walker_class.py
@@ -157,8 +157,6 @@ class Walker:
         self.__reset_direction()
         print(f'prev: {previous_position}, pos: {self.position}')
 
-        case_i = self.__get_case_i(previous_position)
-
         if self.position == '+' and previous_position in '-X':
             self.__direction['up'] = self.__test_up()
             self.__direction['down'] = self.__test_down()
@@ -186,46 +184,6 @@ class Walker:
                 self.__direction['up'] = self.__test_up()
             elif self.__position['row'] > self.__position['prev_row']:
                 self.__direction['down'] = self.__test_down()
-
-    def __get_case_i(self, previous_position) -> int:
-        """
-        Get case i base on the current position vs previous_position
-        :param previous_position:
-        :return:
-        """
-        i: int = 0
-
-        if self.position == '+' and previous_position in '-X':
-            i = 1
-
-        if self.position == '+' and previous_position == '|':
-            i = 2
-
-        if self.position == previous_position == '+' \
-                and self.__position['col'] == self.__position['prev_col']:
-            i = 3
-
-        if self.position == previous_position == '+' \
-                and self.__position['row'] == self.__position['prev_row']:
-            i = 4
-
-        if self.position == '-' and previous_position in '-X+' \
-                and self.__position['col'] < self.__position['prev_col']:
-            i = 5
-
-        if self.position == '-' and previous_position in '-X+' \
-                and self.__position['col'] > self.__position['prev_col']:
-            i = 6
-
-        if self.position == '|' and previous_position in '|X+' \
-                and self.__position['row'] < self.__position['prev_row']:
-            i = 7
-
-        if self.position == '|' and previous_position in '|X+' \
-                and self.__position['row'] > self.__position['prev_row']:
-            i = 8
-
-        return i
 
     def __test_up(self) -> bool:
         row: int = self.__position['row']
