0525e8ac64dbc558b77e085dd69c750fac4a93b5
Egor Kostan <igorkostan@gmail.com>
2024-11-23 20:56:50 -0800

    Sums of Parts

---
+++ kyu_6/sums_of_parts/solution.py
@@ -0,0 +1,15 @@
+"""
+Test for -> Sums of Parts
+Created by Egor Kostan.
+GitHub: https://github.com/ikostan
+"""
+
+
+def parts_sums(ls: list) -> list:
+    """
+    The function parts_sums will take as parameter a list ls
+    and return a list of the sums of its parts.
+    :param ls:
+    :return:
+    """
+    pass
