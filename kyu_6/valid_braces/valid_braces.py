b872cc76220d487db7f37b09afc6621626a4e147
Egor Kostan <igorkostan@gmail.com>
2024-11-24 22:10:20 -0800

    Update valid_braces.py

--- kyu_6/valid_braces/valid_braces.py
+++ kyu_6/valid_braces/valid_braces.py
@@ -1,8 +1,17 @@
 """
-Test for -> Unique In Order
+Test for -> Valid Braces
 Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
+
+BRACES: dict = {
+    '(': ')',
+    ')': '(',
+    '[': ']',
+    ']': '[',
+    '{': '}',
+    '}': '{',
+}
 
 
 def valid_braces(string: str) -> bool:
@@ -13,6 +22,19 @@ def valid_braces(string: str) -> bool:
     :param string:
     :return:
     """
-    result: bool = False
+    # Calc length of the input string
+    len_str: int = len(string)
+    # If the length is not even number -> return False
+    if len_str % 2 > 0:
+        return False
 
-    return result
+    # calculate indexes in order to process matching pairs
+    a, b = (len_str // 2) - 1, len_str // 2
+    while a > -1 and b < len_str:
+        # Pair does not match, fails the test
+        if BRACES[string[a]] != string[b]:
+            return False
+        a -= 1
+        b += 1
+
+    return True
