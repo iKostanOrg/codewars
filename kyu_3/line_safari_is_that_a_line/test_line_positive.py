c90e7180e4365752c7846f4d52a5b7d1da01ef92
Egor Kostan <igorkostan@gmail.com>
2024-12-04 19:52:22 -0800

    Update test_line_positive.py

--- kyu_3/line_safari_is_that_a_line/test_line_positive.py
+++ kyu_3/line_safari_is_that_a_line/test_line_positive.py
@@ -1,6 +1,7 @@
 """
-Testing Line Safari functionality
-Positive test cases
+Testing Line Safari functionality.
+
+Positive test cases.
 Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
@@ -27,13 +28,11 @@ from kyu_3.line_safari_is_that_a_line.line_safari import line
     name='Source/Kata')
 @pytest.mark.skip(reason="The solution is not ready")
 class LinePositiveTestCase(unittest.TestCase):
-    """
-    Testing Line Safari functionality.
-    """
+    """Testing Line Safari functionality."""
+
     def test_line_positive(self):
-        """
-        Positive test cases.
-        """
+        """Positive test cases."""
+
         allure.dynamic.title("Testing Line Safari functionality - POSITIVE")
         # pylint: disable-msg=R0801
         allure.dynamic.severity(allure.severity_level.NORMAL)
