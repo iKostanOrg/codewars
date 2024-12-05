5a0fbee57e99ea0b33271774f7521178fcd21968
Egor Kostan <igorkostan@gmail.com>
2024-12-04 20:12:08 -0800

    Update test_walker.py
    ./kyu_3/line_safari_is_that_a_line/test_walker.py:31:1: W293 blank line contains whitespace

--- kyu_3/line_safari_is_that_a_line/test_walker.py
+++ kyu_3/line_safari_is_that_a_line/test_walker.py
@@ -28,7 +28,7 @@ from kyu_3.line_safari_is_that_a_line.walker_class import Walker
 # pylint: enable-msg=R0801
 class WalkerClassTestCase(unittest.TestCase):
     """Testing Walker class."""
-    
+
     def test_starting_position_from_positives(self):
         """
         Testing Walker class.
