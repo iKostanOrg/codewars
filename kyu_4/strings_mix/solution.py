2737838bcb3cf319baec52683bc61188222de8a6
Egor Kostan <igorkostan@gmail.com>
2024-12-07 05:31:18 -0800

    Update solution.py
    ./kyu_4/strings_mix/solution.py:16:1: W293 blank line contains whitespace
        """
        Mix function.
    
        Given two strings s1 and s2, we want to visualize
        how different the two strings are. We will only
        take into account the lowercase letters (a to z).
        First let us count the frequency of each lowercase
        letters in s1 and s2.
        :param s1: string a
        :param s2: string b
        :return: the difference between two strings
        """

--- kyu_4/strings_mix/solution.py
+++ kyu_4/strings_mix/solution.py
@@ -13,7 +13,7 @@ LOWERCASE = string.ascii_lowercase
 def mix(s1: str, s2: str) -> str:
     """
     Mix function.
-    
+
     Given two strings s1 and s2, we want to visualize
     how different the two strings are. We will only
     take into account the lowercase letters (a to z).
