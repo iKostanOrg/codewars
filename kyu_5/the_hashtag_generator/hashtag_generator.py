9914dca16c2a576c4203fb71a2f0e1860a6b42e3
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-13 07:14:44 -0800

    Update hashtag_generator.py
    looking for now-outdated files... /home/docs/checkouts/readthedocs.org/user_builds/codewars/checkouts/555/kyu_5/the_hashtag_generator/hashtag_generator.py:docstring of kyu_5.the_hashtag_generator.hashtag_generator.generate_hashtag:6: WARNING: Enumerated list ends without a blank line; unexpected unindent. [docutils]

--- kyu_5/the_hashtag_generator/hashtag_generator.py
+++ kyu_5/the_hashtag_generator/hashtag_generator.py
@@ -14,6 +14,7 @@ def generate_hashtag(word: str) -> bool | str:
     2. All words must have their first letter capitalized.
     3. If the final result is longer than 140 chars it must return false.
     4. If the input or the result is an empty string it must return false.
+    
     :param word: str
     :return:
     """
@@ -23,5 +24,4 @@ def generate_hashtag(word: str) -> bool | str:
     result: str = ''.join(string.capitalize() for string in word.split(' ')
                           if string.strip() != '')
     result = '#' + result
-
     return False if len(result) > 140 else result
