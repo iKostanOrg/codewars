08468c0c4066c652df324e5ec6e9ef819918529f
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-13 01:38:28 -0800

    Update alphanumeric.py
    kyu_5/not_very_secure/alphanumeric.py:10 in public function `alphanumeric`:
            D202: No blank lines allowed after function docstring (found 1)
    
            There's no blank line either before or after the docstring unless directly
            followed by an inner function or class.
    
    kyu_5/not_very_secure/alphanumeric.py:10 in public function `alphanumeric`:
            D403: First word of the first line should be properly capitalized ('Alphanumeric', not 'alphanumeric')
    
            The [first line of a] docstring is a phrase ending in a period.

--- kyu_5/not_very_secure/alphanumeric.py
+++ kyu_5/not_very_secure/alphanumeric.py
@@ -11,18 +11,13 @@ def alphanumeric(password: str) -> bool:
     alphanumeric function.
 
     The string has the following conditions to be alphanumeric:
-
     1. At least one character ("" is not valid)
-
     2. Allowed characters are uppercase or
     lowercase latin letters and digits from 0 to 9
-
     3. No whitespaces or underscore
-
     :param password: string
     :return: bool
     """
-
     if not password:
         return False
 
