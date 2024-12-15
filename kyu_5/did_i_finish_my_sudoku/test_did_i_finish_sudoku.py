252660de872cdbc43d6aac5f682a4adbbb8413df
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-15 06:09:31 -0800

    Update test_did_i_finish_sudoku.py

--- kyu_5/did_i_finish_my_sudoku/test_did_i_finish_sudoku.py
+++ kyu_5/did_i_finish_my_sudoku/test_did_i_finish_sudoku.py
@@ -10,6 +10,7 @@ GitHub: https://github.com/ikostan
 
 import unittest
 import allure
+from parameterized import parameterized
 from utils.log_func import print_log
 from kyu_5.did_i_finish_my_sudoku.is_sudoku_done \
     import done_or_not
@@ -38,7 +39,38 @@ from kyu_5.did_i_finish_my_sudoku.is_sudoku_done \
 class DidIFinishedSudokuTestCase(unittest.TestCase):
     """Testing done_or_not function."""
 
-    def test_done_or_not(self):
+    @parameterized.expand([
+        ([[1, 3, 2, 5, 7, 9, 4, 6, 8],
+          [4, 9, 8, 2, 6, 1, 3, 7, 5],
+          [7, 5, 6, 3, 8, 4, 2, 1, 9],
+          [6, 4, 3, 1, 5, 8, 7, 9, 2],
+          [5, 2, 1, 7, 9, 3, 8, 4, 6],
+          [9, 8, 7, 4, 2, 6, 5, 3, 1],
+          [2, 1, 4, 9, 3, 5, 6, 8, 7],
+          [3, 6, 5, 8, 1, 7, 9, 2, 4],
+          [8, 7, 9, 6, 4, 2, 1, 5, 3]],
+         'Finished!'),
+        ([[1, 3, 2, 5, 7, 9, 4, 6, 8],
+          [4, 9, 8, 2, 6, 1, 3, 7, 5],
+          [7, 5, 6, 3, 8, 4, 2, 1, 9],
+          [6, 4, 3, 1, 5, 8, 7, 9, 2],
+          [5, 2, 1, 7, 9, 3, 8, 4, 6],
+          [9, 8, 7, 4, 2, 6, 5, 3, 1],
+          [2, 1, 4, 9, 3, 5, 6, 8, 7],
+          [3, 6, 5, 8, 1, 7, 9, 2, 4],
+          [8, 7, 9, 6, 4, 2, 1, 3, 5]],
+         'Try again!'),
+        ([[1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [2, 3, 4, 5, 6, 7, 8, 9, 1],
+          [3, 4, 5, 6, 7, 8, 9, 1, 2],
+          [4, 5, 6, 7, 8, 9, 1, 2, 3],
+          [5, 6, 7, 8, 9, 1, 2, 3, 4],
+          [6, 7, 8, 9, 1, 2, 3, 4, 5],
+          [7, 8, 9, 1, 2, 3, 4, 5, 6],
+          [8, 9, 1, 2, 3, 4, 5, 6, 7],
+          [9, 1, 2, 3, 4, 5, 6, 7, 8]],
+         'Try again!')])
+    def test_done_or_not(self, board, expected):
         """
         Testing done_or_not function.
 
@@ -58,41 +90,9 @@ class DidIFinishedSudokuTestCase(unittest.TestCase):
             "<p>Testing a function done_or_not/DoneOrNot passing a "
             "board (list[list_lines]) as parameter. If the board is "
             "valid return 'Finished!', otherwise return 'Try again!'</p>")
-        test_data: tuple = (
-            ([[1, 3, 2, 5, 7, 9, 4, 6, 8],
-              [4, 9, 8, 2, 6, 1, 3, 7, 5],
-              [7, 5, 6, 3, 8, 4, 2, 1, 9],
-              [6, 4, 3, 1, 5, 8, 7, 9, 2],
-              [5, 2, 1, 7, 9, 3, 8, 4, 6],
-              [9, 8, 7, 4, 2, 6, 5, 3, 1],
-              [2, 1, 4, 9, 3, 5, 6, 8, 7],
-              [3, 6, 5, 8, 1, 7, 9, 2, 4],
-              [8, 7, 9, 6, 4, 2, 1, 5, 3]],
-             'Finished!'),
-            ([[1, 3, 2, 5, 7, 9, 4, 6, 8],
-              [4, 9, 8, 2, 6, 1, 3, 7, 5],
-              [7, 5, 6, 3, 8, 4, 2, 1, 9],
-              [6, 4, 3, 1, 5, 8, 7, 9, 2],
-              [5, 2, 1, 7, 9, 3, 8, 4, 6],
-              [9, 8, 7, 4, 2, 6, 5, 3, 1],
-              [2, 1, 4, 9, 3, 5, 6, 8, 7],
-              [3, 6, 5, 8, 1, 7, 9, 2, 4],
-              [8, 7, 9, 6, 4, 2, 1, 3, 5]],
-             'Try again!'),
-            ([[1, 2, 3, 4, 5, 6, 7, 8, 9],
-              [2, 3, 4, 5, 6, 7, 8, 9, 1],
-              [3, 4, 5, 6, 7, 8, 9, 1, 2],
-              [4, 5, 6, 7, 8, 9, 1, 2, 3],
-              [5, 6, 7, 8, 9, 1, 2, 3, 4],
-              [6, 7, 8, 9, 1, 2, 3, 4, 5],
-              [7, 8, 9, 1, 2, 3, 4, 5, 6],
-              [8, 9, 1, 2, 3, 4, 5, 6, 7],
-              [9, 1, 2, 3, 4, 5, 6, 7, 8]],
-             'Try again!'))
         # pylint: enable-msg=R0801
-        for board, expected in test_data:
+        with allure.step(f"Enter sudoku: {board} "
+                         f"and verify the output: {expected}."):
             result: str = done_or_not(board)
-
-            with allure.step("Enter sudoku and verify the output."):
-                print_log(expected=expected, result=result)
-                self.assertEqual(expected, result)
+            print_log(expected=expected, result=result)
+            self.assertEqual(expected, result)
