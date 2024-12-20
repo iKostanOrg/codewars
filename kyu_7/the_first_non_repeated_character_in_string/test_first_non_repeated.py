538f3399d25381467132f31529c5de78b292ba84
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-20 05:41:44 -0800

    # The First Non Repeated Character In A String

--- kyu_7/the_first_non_repeated_character_in_string/test_first_non_repeated.py
+++ kyu_7/the_first_non_repeated_character_in_string/test_first_non_repeated.py
@@ -1,5 +1,6 @@
 """
-Test for -> The First Non Repeated Character In A String
+Test for -> The First Non-Repeated Character In A String.
+
 Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
@@ -8,6 +9,7 @@ GitHub: https://github.com/ikostan
 
 import unittest
 import allure
+from parameterized import parameterized
 from utils.log_func import print_log
 from kyu_7.the_first_non_repeated_character_in_string.first_non_repeated \
     import first_non_repeated
@@ -28,49 +30,43 @@ from kyu_7.the_first_non_repeated_character_in_string.first_non_repeated \
     name='Source/Kata')
 # pylint: enable-msg=R0801
 class FirstNonRepeatedTestCase(unittest.TestCase):
-    """
-    Testing first_non_repeated function
-    """
+    """Testing first_non_repeated function."""
 
-    def test_first_non_repeated(self):
+    @parameterized.expand([
+        ("test", 'e'),
+        ("teeter", 'r'),
+        ("1122321235121222", '5'),
+        ('1122321235121222dsfasddssdfa112232123sdfasdfasdf11'
+         '22321235121222dsfasddssdfa112232123sdfasdfasdf1122'
+         '321231122321235121222dsfasddssdfa112232123sdfasdfa'
+         'sdf1122321231122321235121222dsfasddssdfa112232123sd'
+         'fasdfasdf1122321231122321235121222dsfasddssdfa11223'
+         '2123sdfasdfasdf1122321231122321235121222dsfasddssdf'
+         'a112232123sdfasdfasdf112232123asddssdfa112232123sdfa'
+         'sdfasdf1122z321231122321235121222dsfasddssdf1122321'
+         '235121222dsfasddssdf1122321235121222dsfasddssdf11223'
+         '21235121222dsfasddssdf1122321235121222dsfasddssdf112'
+         'p2321235121222dsfasddssdf1122321235121222dsfasddssdf', 'z'),
+        ('ogmhrsoqiklqfmhgnpjsrikmnlpfj', None),
+        ('knioolrpnutskmqmhqtriipjjushl', None)])
+    def test_first_non_repeated(self, s, expected):
         """
-        Testing first_non_repeated function
+        Testing first_non_repeated function with various test data.
+
         :return:
         """
         # pylint: disable-msg=R0801
-        allure.dynamic.title("Testing first_non_repeated "
-                             "function with various inputs")
+        allure.dynamic.title("Testing first_non_repeated function with various inputs")
         allure.dynamic.severity(allure.severity_level.NORMAL)
         allure.dynamic.description_html(
             '<h3>Codewars badge:</h3>'
-            '<img src="https://www.codewars.com/users/myFirstCode'
-            '/badges/large">'
+            '<img src="'
+            'https://www.codewars.com/users/myFirstCode/badges/large'
+            '">'
             '<h3>Test Description:</h3>'
             "<p></p>")
         # pylint: enable-msg=R0801
-        with allure.step("Enter test string and verify the output"):
-            test_data: tuple = (
-                ("test", 'e'),
-                ("teeter", 'r'),
-                ("1122321235121222", '5'),
-                ('1122321235121222dsfasddssdfa112232123sdfasdfasdf11'
-                 '22321235121222dsfasddssdfa112232123sdfasdfasdf1122'
-                 '321231122321235121222dsfasddssdfa112232123sdfasdfa'
-                 'sdf1122321231122321235121222dsfasddssdfa112232123sd'
-                 'fasdfasdf1122321231122321235121222dsfasddssdfa11223'
-                 '2123sdfasdfasdf1122321231122321235121222dsfasddssdf'
-                 'a112232123sdfasdfasdf112232123asddssdfa112232123sdfa'
-                 'sdfasdf1122z321231122321235121222dsfasddssdf1122321'
-                 '235121222dsfasddssdf1122321235121222dsfasddssdf11223'
-                 '21235121222dsfasddssdf1122321235121222dsfasddssdf112'
-                 'p2321235121222dsfasddssdf1122321235121222dsfasddssdf', 'z'),
-                ('ogmhrsoqiklqfmhgnpjsrikmnlpfj', None),
-                ('knioolrpnutskmqmhqtriipjjushl', None))
-
-            for s, expected in test_data:
-
-                print_log(s=s,
-                          expected=expected)
-
-                self.assertEqual(expected,
-                                 first_non_repeated(s))
+        with allure.step(f"Enter test string: {s} "
+                         f"and verify the expected output: {expected}."):
+            print_log(s=s, expected=expected)
+            self.assertEqual(expected, first_non_repeated(s))
