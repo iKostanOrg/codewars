0c53145530fb53f0e04976b7ea7c4d71080b3e86
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-20 06:26:00 -0800

    Update two_decimal_places.py

--- kyu_7/formatting_decimal_places_1/two_decimal_places.py
+++ kyu_7/formatting_decimal_places_1/two_decimal_places.py
@@ -8,16 +8,10 @@ GitHub: https://github.com/ikostan
 
 def two_decimal_places(number) -> float:
     """
-    Formatting decimal places #1.
+    Format decimal places #1.
 
     Each floating-point number should be formatted that
     only the first two decimal places are returned.
-
-    You don't need to check whether the input is a valid
-    number because only valid numbers are used in the tests.
-
-    Don't round the numbers! Just cut them after two decimal
-    places!
     :param number:
     :return: float
     """
