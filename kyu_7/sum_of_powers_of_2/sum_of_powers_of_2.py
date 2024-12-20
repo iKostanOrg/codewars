4237e27858e73c88e5400619142b2352cfb74f1c
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-20 05:04:45 -0800

    # Sum of powers of 2

--- kyu_7/sum_of_powers_of_2/sum_of_powers_of_2.py
+++ kyu_7/sum_of_powers_of_2/sum_of_powers_of_2.py
@@ -1,5 +1,6 @@
 """
-Solution for -> Sum of powers of 2
+Solution for -> Sum of powers of 2.
+
 Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
@@ -9,22 +10,21 @@ from typing import List
 
 def powers(n: int) -> list:
     """
-    Return an array of numbers
-    (that are a power of 2)
-    for which the input "n" is the sum
+    Powers function.
+
+    Return an array of numbers (that are a power of 2)
+    for which the input "n" is the sum.
     :param n:
     :return:
     """
     lst: list = []
     power: int = 0
-
     while (2 ** power) <= n:
         lst.append(2 ** power)
         power += 1
 
     i: int = -1
     result: List[int] = []
-
     while sum(result) != n:
         if sum(result) + lst[i] <= n:
             result.append(lst[i])
