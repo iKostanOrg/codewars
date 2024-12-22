4e7cae11fa9d7eacc68c8ab7d87aa930e32e64f0
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-22 14:14:21 -0800

    ALTerNAtiNG CaSe

--- kyu_8/alternating_case/alternating_case.py
+++ kyu_8/alternating_case/alternating_case.py
@@ -1,5 +1,6 @@
 """
-altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe.
+
 Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
@@ -7,9 +8,11 @@ GitHub: https://github.com/ikostan
 
 def to_alternating_case(string: str) -> str:
     """
+    Alternating case.
+
     each lowercase letter becomes uppercase and
-    each uppercase letter becomes lowercase
-    :param string:
-    :return:
+    each uppercase letter becomes lowercase.
+    :param string: str
+    :return: str
     """
     return ''.join((char.upper() if char.islower() else char.lower()) for char in string)
