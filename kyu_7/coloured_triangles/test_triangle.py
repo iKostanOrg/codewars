c984b53130c64d354b0e60569f31b5f3011ddc48
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-25 03:06:13 -0800

    Complete The Pattern #5 - Even Ladder

--- kyu_7/coloured_triangles/test_triangle.py
+++ kyu_7/coloured_triangles/test_triangle.py
@@ -56,6 +56,16 @@ class TriangleTestCase(unittest.TestCase):
 
         :return:
         """
+        # pylint: disable-msg=R0801
+        allure.dynamic.title("Basic test case for triangle func.")
+        allure.dynamic.severity(allure.severity_level.NORMAL)
+        allure.dynamic.description_html(
+            '<h3>Codewars badge:</h3>'
+            '<img src="https://www.codewars.com/users/myFirstCode'
+            '/badges/large">'
+            '<h3>Test Description:</h3>'
+            "<p></p>")
+        # pylint: enable-msg=R0801
         with allure.step(f"Enter test string: {string} "
                          f"and verify the output: {expected}"):
             result = triangle(string)
