19e8f6320996fbf7f588524035a8fbae729fb444
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-26 07:52:11 -0800

    Sourcery refactoring id: lift-return-into-if¶
    Explanation:¶
    This is a quick way to streamline code slightly. Where a value is set on each branch of an if and then immediately returned, instead return it directly from each branch.
    
    This has removed an unnecessary intermediate variable which we had to mentally track.

--- kyu_8/dalmatians_101_squash_bugs/solution.py
+++ kyu_8/dalmatians_101_squash_bugs/solution.py
@@ -19,12 +19,10 @@ def how_many_dalmatians(number: int) -> str:
                   "101 DALMATIONS!!!"]
 
     if number <= 10:
-        respond = dogs[0]
+        return dogs[0]
     elif number <= 50:
-        respond = dogs[1]
+        return dogs[1]
     elif number == 101:
-        respond = dogs[3]
-    else:
-        respond = dogs[2]
+        return dogs[3]
 
-    return respond
+    return dogs[2]
