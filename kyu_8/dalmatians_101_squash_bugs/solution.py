3daf2855e532011ae1387e95c765960e3237d818
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-26 07:55:10 -0800

    Update solution.py
    kyu_8/dalmatians_101_squash_bugs/solution.py:21:4: R1705: Unnecessary "elif" after "return", remove the leading "el" from "elif" (no-else-return)

--- kyu_8/dalmatians_101_squash_bugs/solution.py
+++ kyu_8/dalmatians_101_squash_bugs/solution.py
@@ -20,9 +20,11 @@ def how_many_dalmatians(number: int) -> str:
 
     if number <= 10:
         return dogs[0]
-    elif number <= 50:
+
+    if number <= 50:
         return dogs[1]
-    elif number == 101:
+
+    if number == 101:
         return dogs[3]
 
     return dogs[2]
