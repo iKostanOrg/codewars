a8f34ca451e7e166c97acccb5f2719a79a6b6955
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-30 06:59:49 -0800

    Update solution.py

--- kyu_7/pointless_farmer/solution.py
+++ kyu_7/pointless_farmer/solution.py
@@ -5,8 +5,10 @@ Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
 
+from typing import Any
 
-def buy_or_sell(pairs: list, harvested_fruit: str) -> str | list:
+
+def buy_or_sell(pairs: list, harvested_fruit: str) -> list[Any]:
     """
     Decide which direction to trade.
 
