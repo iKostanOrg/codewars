2b6c8b5884bd31260dbb97363e02210bc357170e
Egor Kostan <igorkostan@gmail.com>
2025-02-05 20:44:47 -0800

    Update calculate.py

--- kyu_7/basic_math_add_or_subtract/calculate.py
+++ kyu_7/basic_math_add_or_subtract/calculate.py
@@ -5,6 +5,10 @@ Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
 
+from asteval import Interpreter
+
+aeval = Interpreter()
+
 CONVERSION: dict = {
     'plus': '+',
     'minus': '-'}
@@ -21,8 +25,9 @@ def calculate(s: str) -> str:
     # pylint: disable=W0123
     # Evaluate a simple mathematical expression using
     # Python's built-in eval (safe subset).
-    allowed_names = {"__builtins__": None}
-    return f'{eval(s, {"__builtins__": None}, allowed_names)}'  # nosec B311
+    # allowed_names = {"__builtins__": None}
+    # return f'{eval(s, {"__builtins__": None}, allowed_names)}'  # nosec B311
+    return f'{aeval.eval(s)}'  # nosec B311
     # pylint: enable=W0123
 
 
@@ -33,7 +38,7 @@ def string_to_math(s: str) -> str:
     :param s: str
     :return: str
     """
-    s = s.lower()
+    s = s.lower().replace(' ', '')
     for key, item in CONVERSION.items():
         if key in s:
             s = s.replace(key, item)
