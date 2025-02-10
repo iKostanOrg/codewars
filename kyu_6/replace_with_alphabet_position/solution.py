2e3abb7f1e8017491757b904233c0f00ede52342
Egor Kostan <igorkostan@gmail.com>
2025-02-09 22:36:40 -0800

    error fixing
    Using config file /home/runner/work/codewars/codewars/.pylintrc
    ************* Module kyu_6.replace_with_alphabet_position.test_replace_with_alphabet_position
    kyu_6/replace_with_alphabet_position/test_replace_with_alphabet_position.py:68:0: C0304: Final newline missing (missing-final-newline)
    ************* Module kyu_6.replace_with_alphabet_position.solution
    kyu_6/replace_with_alphabet_position/solution.py:13:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)
    kyu_6/replace_with_alphabet_position/solution.py:19:0: W0311: Bad indentation. Found 1 spaces, expected 4 (bad-indentation)

--- kyu_6/replace_with_alphabet_position/solution.py
+++ kyu_6/replace_with_alphabet_position/solution.py
@@ -4,16 +4,17 @@ Solution for -> Replace With Alphabet Position
 Created by Egor Kostan.
 GitHub: https://github.com/ikostan
 """
+
 import string
 
 alphabet: str = string.ascii_lowercase
 
 
 def alphabet_position(text: str) -> str:
-	"""
+    """
 	Replace every letter with its position in the alphabet.
 
 	:param text: str
 	:return: str
-	"""
-	return ' '.join([str(alphabet.index(char.lower()) + 1) for char in text if char.isalpha()])
+    """
+    return ' '.join([str(alphabet.index(char.lower()) + 1) for char in text if char.isalpha()])
