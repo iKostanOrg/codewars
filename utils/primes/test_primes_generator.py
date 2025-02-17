69e99af1bd529d9fda78718c983d2bdd55afb944
Egor Kostan <igorkostan@gmail.com>
2025-02-16 18:03:05 -0800

    Update test_primes_generator.py

--- utils/primes/test_primes_generator.py
+++ utils/primes/test_primes_generator.py
@@ -30,9 +30,9 @@ class GenPrimesTestCase(unittest.TestCase):
 
     @parameterized.expand([
         ([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
-         53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
-         109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
-         173, 179, 181, 191, 193, 197, 199],),])
+          53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
+          109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+          173, 179, 181, 191, 193, 197, 199],), ])
     def test_gen_primes_positive(self, test_data):
         """
         Positive test cases for gen_primes function testing
@@ -61,9 +61,9 @@ class GenPrimesTestCase(unittest.TestCase):
 
     @parameterized.expand([
         ([3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-        61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
-        131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
-        193, 197, 199],),])
+          61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
+          131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+          193, 197, 199],), ])
     def test_gen_primes_negative(self, test_data):
         """
         Negative test cases for gen_primes function testing
